1338a6f64c5e8a45c3d28943365927bfd5943fb3
Lars Doelle <lars.doelle@on-line.de>
2000-01-14 00:33:08 +0000

    adjustments
    svn path=/trunk/kdebase/konsole/; revision=38278

--- tests/utf8.sh
+++ tests/utf8.sh
@@ -1,11 +1,11 @@
-#!/bin/sh --
+#!/bin/bash --
 #
 # Switch utf-8 mode
 #
 #-------------------------------------------------------------------------
 
 case $1 in
-  on) echo "%G""UTF-8 on";;
-  off) echo "%@""UTF-8 off";;
+  on) echo $'\033%G'"UTF-8 on";;
+  off) echo $'\033%@'"UTF-8 off";;
   *) echo "usage: $0 [on|off]";;
 esac
