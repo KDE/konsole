7029de3d871d8e1148ee9ddec84a625bcda5b8b4
Michael Matz <matz@kde.org>
2000-06-10 19:47:23 +0000

    XTerm title hack: don't activate for arg==1
    svn path=/trunk/kdebase/konsole/; revision=52837

--- src/TEmuVt102.C
+++ src/TEmuVt102.C
@@ -304,7 +304,9 @@ void TEmuVt102::XtermHack()
   QChar *str = new QChar[ppos-i-2];
   for (int j = 0; j < ppos-i-2; j++) str[j] = pbuf[i+1+j];
   QString unistr(str,ppos-i-2);
-  if (arg <= 2) emit changeTitle(arg,unistr);
+  // arg == 1 doesn't change the title. In XTerm it only changes the icon name
+  // (btw: arg=0 changes title and icon, arg=1 only icon, arg=2 only title
+  if (arg == 0 || arg == 2) emit changeTitle(arg,unistr);
   delete str;
 }
 
