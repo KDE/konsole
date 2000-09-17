c4dcf0b8f008d8e7d2461c7c13308b822ba563f2
Lars Doelle <lars.doelle@on-line.de>
2000-09-17 20:55:14 +0000

    file moved
    svn path=/trunk/kdebase/konsole/; revision=64192

--- tests/colortest.sh
+++ tests/colortest.sh
@@ -1,10 +1,14 @@
-#!/bin/sh --
+#!/bin/bash --
 #
 # display ANSI colours and test bold/blink attributes
 # orginates from Eterm distribution
 #-------------------------------------------------------------------------
 
-echo ""; echo "[m"
+ESC=$'\x1b'
+CSI="${ESC}["
+RST="${CSI}m"
+
+echo ""; echo "${RST}"
 echo "       40      41      42      43      44      45      46      47      49"
 echo "       40      41      42      43      44      45      46      47      49"
 for fg in 30 31 32 33 34 35 36 37 39 90 91 92 93 94 95 96 97
@@ -15,10 +19,10 @@ do
     l4="    ";
     for bg in 40 41 42 43 44 45 46 47 49
     do
-	l1="${l1}[${fg};${bg}m Normal [m"
-	l2="${l2}[${fg};${bg};1m Bold   [m"
-	l3="${l3}[${fg};${bg};5m Blink  [m"
-	l4="${l4}[${fg};${bg};1;5m Bold!  [m"
+	l1="${l1}${CSI}${fg};${bg}m Normal ${RST}"
+	l2="${l2}${CSI}${fg};${bg};1m Bold   ${RST}"
+	l3="${l3}${CSI}${fg};${bg};5m Blink  ${RST}"
+	l4="${l4}${CSI}${fg};${bg};1;5m Bold!  ${RST}"
     done
     echo "$l1"
     echo "$l2"
