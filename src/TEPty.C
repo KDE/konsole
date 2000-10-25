599b89036c73cc7607d7351ab320e7a2613857b0
George Staikos <staikos@kde.org>
2000-10-25 18:57:46 +0000

    Re-ordered an include file to get it to compile on HP-UX.  It needs them in this specific order.
    svn path=/trunk/kdebase/konsole/; revision=68886

--- src/TEPty.C
+++ src/TEPty.C
@@ -124,18 +124,18 @@ extern "C" {
 
 #include <signal.h>
 
-#if !defined(__osf__)
-#ifdef HAVE_TERMIO_H
-/* needed at least on AIX */
-#include <termio.h>
-#endif
-#endif
 #ifdef HAVE_TERMIOS_H
 /* for HP-UX (some versions) the extern C is needed, and for other
    platforms it doesn't hurt */
 extern "C" {
 #include <termios.h>
 }
+#endif
+#if !defined(__osf__)
+#ifdef HAVE_TERMIO_H
+/* needed at least on AIX */
+#include <termio.h>
+#endif
 #endif
 //#include <time.h>
 #include <unistd.h>
