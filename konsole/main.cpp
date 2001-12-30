8e0066d499693d66a35c5d207ec7529a27569a54
Stephan Binner <binner@kde.org>
2001-12-30 09:42:47 +0000

    Changed parameter descriptions.
    svn path=/trunk/kdebase/konsole/; revision=129126

--- konsole/main.cpp
+++ konsole/main.cpp
@@ -33,12 +33,12 @@ static const char *description =
 //   { "T <title>",       0, 0 },
 static KCmdLineOptions options[] =
 {
-   { "name <name>",  I18N_NOOP("Set Window Class"), 0 },
+   { "name <name>",  I18N_NOOP("Set window class"), 0 },
    { "ls",    I18N_NOOP("Start login shell"), 0 },
    { "T <title>",       I18N_NOOP("Set the window title"), 0 },
    { "tn <terminal>",   I18N_NOOP("Specify terminal type as set in the TERM\nenvironment variable"), "xterm" },
    { "xwin",            I18N_NOOP("ignored"), 0 },
-   { "nohist",          I18N_NOOP("Do not save lines in scroll-back buffer"), 0 },
+   { "nohist",          I18N_NOOP("Do not save lines in history"), 0 },
    { "nomenubar",       I18N_NOOP("Do not display menubar"), 0 },
    { "notoolbar",       I18N_NOOP("Do not display toolbar"), 0 },
    { "noframe",         I18N_NOOP("Do not display frame"), 0 },
