840f40705f039a0c468cc5946fd373fb234a743f
Stephan Binner <binner@kde.org>
2002-02-10 10:51:09 +0000

    Changes to please some translators:  "Name" is used as session description for "New" button in toolbar and panel "Terminal Sessions" button, eg "Shell".  "Comment" is used as session description in "Session" menu, eg "New Shell".  i18n("New ") is only used/prepended if "Comment" is missing, eg user created session types with session editor.
    svn path=/trunk/kdebase/konsole/; revision=135751

--- konsole/konsole.cpp
+++ konsole/konsole.cpp
@@ -1867,7 +1867,10 @@ void Konsole::addSessionCommand(const QString &path)
   }
   QString icon = co->readEntry("Icon", "openterm");
   m_toolbarSessionsCommands->insertItem( SmallIconSet( icon ), txt, ++cmd_serial );
-  m_session->insertItem( SmallIconSet( icon ), txt.prepend(i18n("New ")), cmd_serial );
+  QString comment = co->readEntry("Comment");
+  if (comment.isEmpty())
+    comment=txt.prepend(i18n("New "));
+  m_session->insertItem( SmallIconSet( icon ), comment, cmd_serial );
   no2command.insert(cmd_serial,co);
 
   int j = filename.findRev('/');
