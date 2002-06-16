751cbb43f304eb9ce036e2db1f6384a0706b9fa1
Stephan Binner <binner@kde.org>
2002-06-16 20:54:33 +0000

    Removed feedSession and sendSession.
    svn path=/branches/KDE_3_0_BRANCH/kdebase/konsole/; revision=161502

--- konsole/sessioniface.h
+++ konsole/sessioniface.h
@@ -11,9 +11,6 @@ class SessionIface : virtual public DCOPObject
     virtual bool closeSession() =0;
     virtual bool sendSignal(int signal) =0;
 
-    virtual void feedSession(const QString &text) =0;
-    virtual void sendSession(const QString &text) =0;
-
     virtual void renameSession(const QString &name) =0;
 };
 
