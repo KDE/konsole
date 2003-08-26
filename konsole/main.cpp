1ff9b8d0709bfdca37d7431efd8d0f961cea4ace
Adriaan de Groot <groot@kde.org>
2003-08-26 19:15:50 +0000

    Make konsole_grantpty work with FreeBSD. This means three things: * don't abort() when functions return something you don't expect.   In particular, ttyname() doesn't work on ptys in FBSD. * Deal with changed gethostname() return values in 5.2. Also   use gethostname() value sensibly. * Don't blindly use fd 3 for _grantpty, but assign a free fd for   the communication with _grantpty, and pass it as an argument.
    caveats: this might leak fd's. passing the fd number on the command
      line might be a security issue.
    
    CCMAIL: kde@freebsd.org
    
    svn path=/branches/KDE_3_1_BRANCH/kdebase/konsole/; revision=245192

--- konsole/main.cpp
+++ konsole/main.cpp
@@ -123,6 +123,7 @@ public:
 };
 
 
+extern void TEPtyInit();
 
 /* --| main |------------------------------------------------------ */
 int main(int argc, char* argv[])
@@ -137,6 +138,7 @@ int main(int argc, char* argv[])
   bool scrollbaron = true;
   QCString wname = PACKAGE;
 
+  TEPtyInit();
 
   KAboutData aboutData( PACKAGE, I18N_NOOP("Konsole"),
     VERSION, description, KAboutData::License_GPL_V2,
