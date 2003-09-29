8f0550d33593a16f5062d179d9078a8b3d1be66f
Adriaan de Groot <groot@kde.org>
2003-09-29 08:52:52 +0000

    Deal with 64-bit time_t
    svn path=/branches/KDE_3_1_BRANCH/kdebase/konsole/; revision=254770

--- konsole/TEPty.cpp
+++ konsole/TEPty.cpp
@@ -624,7 +624,14 @@ int TEPty::makePty(bool _addutmp)
   if (strncmp(str_ptr, "/dev/", 5) == 0)
        str_ptr += 5;
   strncpy(l_struct.ut_line, str_ptr, UT_LINESIZE);
-  time(&l_struct.ut_time);
+
+  // Handle 64-bit time_t properly, where it may be larger
+  // than the integral type of ut_time.
+  {
+      time_t ut_time_temp;
+      time(&ut_time_temp);
+      l_struct.ut_time=ut_time_temp;
+  }
 
   login(&l_struct);
 #endif
