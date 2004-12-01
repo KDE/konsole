8eb3b8e3f36fb093bc801086b343ced4ac3b973b
Waldo Bastian <bastian@kde.org>
2004-12-01 17:05:30 +0000

    KIOSK: Don't crash with shell_access=false, regression caused by r1.472.2.3 BUG: 94224
    svn path=/branches/KDE_3_3_BRANCH/kdebase/konsole/; revision=367658

--- konsole/konsole.cpp
+++ konsole/konsole.cpp
@@ -2381,11 +2381,11 @@ void Konsole::addSession(TESession* s)
     createSessionTab(te, SmallIconSet(s->IconName()), newTitle);
     setSchema(s->schemaNo());
     tabwidget->setCurrentPage(tabwidget->count()-1);
-      disableMasterModeConnections(); // no duplicate connections, remove old
-      enableMasterModeConnections();
-    }
-    if( tabwidget )
-    m_removeSessionButton->setEnabled(tabwidget->count()>1);
+    disableMasterModeConnections(); // no duplicate connections, remove old
+    enableMasterModeConnections();
+    if(m_removeSessionButton)
+      m_removeSessionButton->setEnabled(tabwidget->count()>1);
+  }
 }
 
 QString Konsole::currentSession()
@@ -2827,8 +2827,8 @@ void Konsole::doneSession(TESession* s)
       rootxpms.remove(s->widget());
     }
     delete s->widget();
-    if( tabwidget )
-    m_removeSessionButton->setEnabled(tabwidget->count()>1);
+    if (m_removeSessionButton)
+      m_removeSessionButton->setEnabled(tabwidget->count()>1);
   }
   session2action.remove(s);
   action2session.remove(ra);
