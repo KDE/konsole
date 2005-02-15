54a43a5ddedddf7f8ae7cbc6a14d553176ffd9cb
Stephan Binner <binner@kde.org>
2005-02-15 10:41:37 +0000

    Add missing copyright
    svn path=/trunk/kdebase/konsole/; revision=389585

--- konsole/konsolebookmarkhandler.h
+++ konsole/konsolebookmarkhandler.h
@@ -1,3 +1,21 @@
+/* This file wass part of the KDE libraries
+    Copyright (C) 2002 Carsten Pfeiffer <pfeiffer@kde.org>
+
+    library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation, version 2.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
 // Born as kdelibs/kio/kfile/kfilebookmarkhandler.h
 
 #ifndef KONSOLEBOOKMARKHANDLER_H
