2f7c2ef906cdecf9389706fa2ef2bab90836a041
Joseph Wenninger <kde@jowenn.at>
2006-02-07 21:55:56 +0000

    back again, snapshot has already changes in kdeui
    svn path=/trunk/KDE/kdebase/konsole/; revision=506933

--- settings/schemaeditor.h
+++ settings/schemaeditor.h
@@ -25,9 +25,9 @@
 #include <kapplication.h>
 #include <qwidget.h>
 //Added by qt3to4:
-#include <QPixmap>
+#include <kpixmap.h>
 #include <QVector>
-class QPixmap;
+class KPixmap;
 class KSharedPixmap;
 
 #include "schemadialog.h"
@@ -71,7 +71,7 @@ class SchemaEditor : public SchemaDialog
 	QVector<int> type; // 0= custom, 1= sysfg, 2=sysbg, 3=rcolor
 	QVector<bool> transparent;
 	QVector<bool> bold;
-	QPixmap pix;
+	KPixmap pix;
 	KSharedPixmap *spix;
 	QString defaultSchema;	
 	bool loaded;
