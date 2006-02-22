41e21b19fccc6215fd50e96eda0787fdbb86a3b0
Nicolas Goutte <nicolasg@snafu.de>
2006-02-22 01:47:34 +0000

    Convert the "messages" targets of kdebase into Messages.sh scripts
    svn path=/trunk/KDE/kdebase/konsole/; revision=512183

---
+++ konsole/Messages.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+rm -f schemas.cpp
+rm -f tips.cpp
+(cd .. && $PREPARETIPS > konsole/tips.cpp)
+for i in ../other/*.schema; do
+  grep "^title" $i | cut -d':' -f2- | sed -e 's#^title \(.*\)$#i18n(\"\1\")#' >> schemas.cpp
+done
+for i in ../other/*.Keytab ../other/*.keytab; do
+  grep "^keyboard" $i | sed -e 's#^keyboard \"\(.*\)\"$#i18n(\"\1\")#' >> schemas.cpp
+done
+$XGETTEXT *.cpp -o $podir/konsole.pot
+rm -f schemas.cpp
+rm -f tips.cpp
