b983c39c7246374a86e238358d7cddce3995ba4a
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2006-04-30 23:23:30 +0000

    Verify that the command is executable for '-e <command>.'
    CCBUG: 125977
    
    svn path=/trunk/KDE/kdebase/apps/konsole/; revision=536013

--- konsole/session.cpp
+++ konsole/session.cpp
@@ -7,6 +7,9 @@
 #include <knotifyclient.h>
 #include <klocale.h>
 #include <kprocio.h>
+#include <krun.h>
+#include <kshell.h>
+#include <kstandarddirs.h>
 
 #include <stdlib.h>
 #include <qfile.h>
@@ -128,8 +131,18 @@ void TESession::changeWidget(TEWidget* w)
 
 void TESession::run()
 {
-  //kDebug(1211) << "Running the session!" << pgm << "\n";
-  //pgm = "pine";
+  // Upon a KPty error, there is no description on what that error was...
+  // Check to see if the given program is executable.
+  QString exec = QFile::encodeName(pgm);
+  exec = KRun::binaryName(exec, false);
+  exec = KShell::tildeExpand(exec);
+  QString pexec = KGlobal::dirs()->findExe(exec);
+  if ( pexec.isEmpty() ) {
+    kdError()<<"can not execute "<<exec<<endl;
+    QTimer::singleShot(1, this, SLOT(done()));
+    return;
+  }
+
   QString appId=kapp->dcopClient()->appId();
 
   QString cwd_save = QDir::currentPath();
