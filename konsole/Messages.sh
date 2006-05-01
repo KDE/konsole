40cf31274118769017c80ae532ce03acdcca247f
Stephan Kulow <coolo@kde.org>
2006-05-01 20:26:19 +0000

    removed messages target
    svn path=/trunk/KDE/kdebase/apps/konsole/; revision=536327

--- konsole/Messages.sh
+++ konsole/Messages.sh
@@ -1,12 +1,11 @@
-#! /usr/bin/env bash
+#! /bin/sh
 rm -f schemas.cpp
-rm -f tips.cpp
 (cd .. && $PREPARETIPS > konsole/tips.cpp)
 for i in ../other/*.schema; do
-  grep "^title" $i | cut -d':' -f2- | sed -e 's#^title \(.*\)$#i18n(\"\1\")#' >> schemas.cpp
+grep "^title" $i | cut -d':' -f2- | sed -e 's#^title \(.*\)$#i18n(\"\1\")#' >> schemas.cpp
 done
 for i in ../other/*.Keytab ../other/*.keytab; do
-  grep "^keyboard" $i | sed -e 's#^keyboard \"\(.*\)\"$#i18n(\"\1\")#' >> schemas.cpp
+grep "^keyboard" $i | sed -e 's#^keyboard \"\(.*\)\"$#i18n(\"\1\")#' >> schemas.cpp
 done
 $XGETTEXT *.cpp -o $podir/konsole.pot
 rm -f schemas.cpp
