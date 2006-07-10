5cb6eb74c44aa65ace54c80660c15111fb193fd7
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2006-07-10 18:36:25 +0000

    Fix Copyright/License issues.
    svn path=/trunk/KDE/kdebase/apps/konsole/; revision=560595

--- konsole/TEmulation.cpp
+++ konsole/TEmulation.cpp
@@ -1,15 +1,23 @@
+/*
+    This file is part of Konsole, an X terminal.
+    Copyright (C) 1996 by Matthias Ettrich <ettrich@kde.org>
+    Copyright (C) 1997,1998 by Lars Doelle <lars.doelle@on-line.de>
 
-/* -------------------------------------------------------------------------- */
-/*                                                                            */
-/*                         Terminal Emulation Decoder                         */
-/*                                                                            */
-/* -------------------------------------------------------------------------- */
-/*                                                                            */
-/* Copyright (c) 1997,1998 by Lars Doelle <lars.doelle@on-line.de>            */
-/*                                                                            */
-/* This file is part of Konsole - an X terminal for KDE                       */
-/*                                                                            */
-/* -------------------------------------------------------------------------- */
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301  USA.
+*/
 
 /*! \class TEmulation
 
