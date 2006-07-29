e3ceeb92780afa75d5c487cb454f3aad3f58348d
Robert Knight <robertknight@gmail.com>
2006-07-29 15:37:24 +0000

    Add email address.
    svn path=/trunk/KDE/kdebase/apps/konsole/; revision=567618

--- konsole/OverlayFrame.h
+++ konsole/OverlayFrame.h
@@ -1,6 +1,6 @@
 /*
     This file is part of Konsole, an X terminal.
-    (C) 2006 Robert Knight
+    Copyright (C) 2006 Robert Knight <robertknight@gmail.com>
     
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
