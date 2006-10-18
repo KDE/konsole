3ba6b7ae466d7b99c16297ca3878d95b7ac662a1
Robert Knight <robertknight@gmail.com>
2006-10-18 13:43:51 +0000

    Add copyright notices and compile.
    svn path=/trunk/KDE/kdebase/apps/konsole/; revision=596786

--- konsole/ViewContainer.cpp
+++ konsole/ViewContainer.cpp
@@ -1,3 +1,29 @@
+/*
+    This file is part of the Konsole Terminal.
+    
+    Copyright (C) 2006 Robert Knight <robertknight@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301  USA.
+*/
+
+// Qt
+#include <QLineEdit>
+#include <QTabWidget>
+
+// Konsole
 #include "ViewContainer.h"
 
 void ViewContainer::addView(QWidget* view)
@@ -47,3 +73,5 @@ QWidget* TabbedViewContainer::activeView() const
 {
     return _tabWidget->widget(_tabWidget->currentIndex());
 }
+
+#include "ViewContainer.moc"
--- konsole/ViewSplitter.h
+++ konsole/ViewSplitter.h
@@ -1,6 +1,31 @@
+/*
+    This file is part of the Konsole Terminal.
+    
+    Copyright (C) 2006 Robert Knight <robertknight@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301  USA.
+*/
 
 #ifndef VIEWSPLITTER_H
 #define VIEWSPLITTER_H
+
+#include <QList>
+#include <QSplitter>
+
+class ViewContainer;
 
 /**
  * A splitter which holds a number of ViewContainer objects and allows
