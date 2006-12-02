f1f49c132af93bf90e54e9970b0836e02d934094
Robert Knight <robertknight@gmail.com>
2006-12-02 13:08:27 +0000

    Correct calculation of colour values in 256-colour mode.  The previous code resulted in colours which were too dim.  Patch by Tom Schumm ( gentoo@phong.org )
    svn path=/branches/KDE/3.5/kdebase/konsole/; revision=609838

--- tests/color-spaces.pl
+++ tests/color-spaces.pl
@@ -47,9 +47,9 @@ for ($fgbg = 38; $fgbg <= 48; $fgbg +=10) {
 
 # now the color cube
 print "Color cube\n";
-for ($green = 0; $green < 256; $green+=43) {
-    for ($red = 0; $red < 256; $red+=43) {
-	for ($blue = 0; $blue < 256; $blue+=43) {
+for ($green = 0; $green < 256; $green+=51) {
+    for ($red = 0; $red < 256; $red+=51) {
+	for ($blue = 0; $blue < 256; $blue+=51) {
             print "\x1b[${fgbg};2;${red};${green};${blue}m::";
 	}
 	print "\x1b[0m ";
@@ -59,7 +59,7 @@ for ($green = 0; $green < 256; $green+=43) {
 
 # now the grayscale ramp
 print "Grayscale ramp:\n";
-for ($gray = 0; $gray < 256; $gray+=10) {
+for ($gray = 8; $gray < 256; $gray+=10) {
     print "\x1b[${fgbg};2;${gray};${gray};${gray}m::";
 }
 print "\x1b[0m\n\n";
