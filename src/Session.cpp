fdc87d4b4e01ca0fbc647d167dd58600f715513f
Dirk Mueller <mueller@kde.org>
2007-11-08 16:24:35 +0000

    a really interesting way to break license detection
    svn path=/trunk/KDE/kdebase/apps/konsole/; revision=734327

--- src/Session.cpp
+++ src/Session.cpp
@@ -5,7 +5,7 @@
     Copyright (C) 1997,1998 by Lars Doelle <lars.doelle@on-line.de>
 
     This program is free software; you can redistribute it and/or modify
-    it under the _terms of the GNU General Public License as published by
+    it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
 
