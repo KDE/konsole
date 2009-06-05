2702920f238f2ff9664644fa9dcc43e0a6b1ab1a
Kevin Kofler <kevin.kofler@chello.at>
2009-06-05 00:27:43 +0000

    Allow flow control to be disabled.  The current session will not be affected.
    CCBUG: 165457
    
    (backport revision 955413 by hindenburg as this works just fine since at least 4.2.2, probably since 4.2.0)
    
    svn path=/branches/KDE/4.2/kdebase/apps/konsole/; revision=977678

--- src/EditProfileDialog.cpp
+++ src/EditProfileDialog.cpp
@@ -71,11 +71,6 @@ EditProfileDialog::EditProfileDialog(QWidget* parent)
     // - Renable in a later KDE 4.x release when this feature works again
     _ui->enableResizeWindowButton->setVisible(false);
     
-#ifdef __GNUC__
-#warning "Re-enable when flow control is working again - bug in KDE 4.1"
-#endif
-    _ui->enableFlowControlButton->setEnabled(false);
-
     // there are various setupXYZPage() methods to load the items
     // for each page and update their states to match the profile
     // being edited.
