676df0c26e5c01652da64690756b74fbe180a080
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2011-07-30 22:17:12 -0400

    New tabs will be placed to the right of the current tab.
    Currently new tabs are placed at the end which is not very convenient.
    
    Patch by Jekyll Wu <adaptee@gmail.com>
    FEATURE: 276872
    FIXED-IN: 4.8

--- src/ViewManager.cpp
+++ src/ViewManager.cpp
@@ -610,7 +610,17 @@ void ViewManager::createView(Session* session)
     while ( containerIter.hasNext() )
     {
         ViewContainer* container = containerIter.next();
-        createView(session,container,-1);
+
+        int index = -1;
+        QWidget* view = activeView();
+
+        if (view)
+        {
+            QList<QWidget*> views = container->views();
+            index = views.indexOf(view) + 1;
+        }
+
+        createView(session, container, index);
     }
 
 }
