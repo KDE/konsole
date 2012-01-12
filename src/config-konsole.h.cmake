1e722af5ab368df8a4156e3123dbb530f4c3a51a
Jekyll Wu <adaptee@gmail.com>
2012-01-12 21:51:09 +0800

    HAVE_XRENDER is not used at all in current code

--- src/config-konsole.h.cmake
+++ src/config-konsole.h.cmake
@@ -4,9 +4,6 @@
 /* Define if you have the XKB extension */
 #cmakedefine HAVE_XKB 1
 
-/* Defined if your system has XRender support */
-#cmakedefine HAVE_XRENDER 1
-
 /* Defined if you have sys/proc.h */
 #cmakedefine HAVE_SYS_PROC_H 1
 
