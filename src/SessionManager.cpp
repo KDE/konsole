053d1f27cda75cd0338d4768d509bff4766366a3
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2012-02-02 10:50:05 -0500

    Use relative paths in config file
        konsolerc uses in 'Favorites Profiles' and 'Profile Shortcuts' the full
        path.  Changing this to relative path (if in a KDE location) allows
        one user can copy then to another.
    
        CCBUG: 283102

--- src/SessionManager.cpp
+++ src/SessionManager.cpp
@@ -657,6 +657,14 @@ void SessionManager::loadShortcuts()
         QString profilePath = iter.value();
 
         ShortcutData data;
+ 
+        // if the file is not an absolute path, look it up
+        QFileInfo fileInfo(profilePath);
+        if (!fileInfo.isAbsolute())
+        {
+            profilePath = KStandardDirs::locate("data","konsole/" + profilePath);
+        }
+
         data.profilePath = profilePath;
 
         _shortcuts.insert(shortcut,data);
@@ -675,8 +683,20 @@ void SessionManager::saveShortcuts()
 
         QString shortcutString = iter.key().toString();
 
-        shortcutGroup.writeEntry(shortcutString,
-                iter.value().profilePath);
+        // if the profile path in "Profile Shortcuts" is an absolute path,
+        // take the profile name
+        QFileInfo fileInfo(iter.value().profilePath);
+        QString profileName;
+        if (fileInfo.isAbsolute())
+        {
+            profileName = fileInfo.fileName();
+        }
+        else
+        {
+            profileName = iter.value().profilePath;
+        }
+
+        shortcutGroup.writeEntry(shortcutString, profileName);
     }    
 }
 void SessionManager::setShortcut(Profile::Ptr profile , 
@@ -752,7 +772,7 @@ void SessionManager::saveFavorites()
 
         Q_ASSERT( _profiles.contains(profile) && profile );
 
-        paths << profile->path();
+        paths << profile->name() + ".profile";
     }
 
     favoriteGroup.writeEntry("Favorites",paths);
