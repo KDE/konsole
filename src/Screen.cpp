695c68027109b352bc05dc198b379912d23cc959
Jekyll Wu <adaptee@gmail.com>
2012-03-05 06:16:54 +0800

    Less unneeded #include

--- src/Screen.cpp
+++ src/Screen.cpp
@@ -23,13 +23,6 @@
 // Own
 #include "Screen.h"
 
-// Standard
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <ctype.h>
-
 // Qt
 #include <QtCore/QTextStream>
 
