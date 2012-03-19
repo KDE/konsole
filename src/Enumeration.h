d5d7928c482d7ef996c15aae190819ccdfe07b77
Jekyll Wu <adaptee@gmail.com>
2012-03-19 13:50:21 +0800

    update header and comment

--- src/Enumeration.h
+++ src/Enumeration.h
@@ -1,30 +1,34 @@
 /*
-  Copyright 2012 Jekyll Wu <adaptee@gmail.com>
+    This file is part of Konsole, KDE's terminal emulator.
 
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License as
-  published by the Free Software Foundation; either version 2 of
-  the License or (at your option) version 3 or any later version
-  accepted by the membership of KDE e.V. (or its successor appro-
-  ved by the membership of KDE e.V.), which shall act as a proxy
-  defined in Section 14 of version 3 of the license.
-This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General Public License for more details.
+    Copyright 2012 by Jekyll Wu <adaptee@gmail.com>
 
-  You should have received a copy of the GNU General Public License
-  along with this program. If not, see http://www.gnu.org/licenses/.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301  USA.
 */
 
 #ifndef ENUMERATION_H
 #define ENUMERATION_H
 
-#include "konsole_export.h"
-
 namespace Konsole
 {
 
+/**
+ * This class serves as a place for putting enum definitions that are
+ * used or referenced in multiple places in the code. Keep it small.
+ */
 class Enum
 {
 public:
