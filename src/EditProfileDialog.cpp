89a17cba376fbc36ed3aa5361a6d492ce5ec83b9
Jekyll Wu <adaptee@gmail.com>
2012-05-08 03:46:58 +0800

    Use customized caption "Select Fixed With Font" for the KFontDialog
    The idea is to give users better hint that Konsole requires fixed-width font,
    so only valid fixed-width fonts are listed. That should help reducing the
    common question: "Why font <xyz> is not listed in konsole?"

--- src/EditProfileDialog.cpp
+++ src/EditProfileDialog.cpp
@@ -1153,6 +1153,7 @@ void EditProfileDialog::showFontDialog()
     QFont currentFont = _ui->fontPreviewLabel->font();
 
     QWeakPointer<KFontDialog> dialog = new KFontDialog(this, KFontChooser::FixedFontsOnly);
+    dialog.data()->setCaption(i18n("Select Fixed With Font"));
     dialog.data()->setFont(currentFont, true);
 
     // TODO (hindenburg): When https://git.reviewboard.kde.org/r/103357 is
