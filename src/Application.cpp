63b489443c9166b268707f4fa86c773568f0f966
Jekyll Wu <adaptee@gmail.com>
2012-05-11 15:50:53 +0800

    No need to check for Qt version higher than 4.6
    It is safe to assume Qt-4.6+ everywhere now. And kdelibs now requires Qt-4.7
    as minimal version .

--- src/Application.cpp
+++ src/Application.cpp
@@ -49,7 +49,7 @@ void Application::init()
 {
     _backgroundInstance = 0;
 
-#if defined(Q_WS_MAC) && QT_VERSION >= 0x040600
+#if defined(Q_WS_MAC)
     // this ensures that Ctrl and Meta are not swapped, so CTRL-C and friends
     // will work correctly in the terminal
     setAttribute(Qt::AA_MacDontSwapCtrlAndMeta);
