4b80a9abfa52e616d9647cefe9b277d2af15a5e6
Jekyll Wu <adaptee@gmail.com>
2012-06-04 21:28:35 +0800

    Use 'sh' as the testing command
    I was stupid enough to assuem zsh was everywhere. But I really hope
    that would become the reality some day.

--- src/tests/PtyTest.cpp
+++ src/tests/PtyTest.cpp
@@ -76,7 +76,7 @@ void PtyTest::testWindowSize()
 void PtyTest::testRunProgram()
 {
     Pty pty;
-    QString program = "zsh";
+    QString program = "sh";
     QStringList arguments ;
     arguments << program;
     QStringList environments;
