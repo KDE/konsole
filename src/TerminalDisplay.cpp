b464d031d5d7483d0964f288357dac0923232e92
Jekyll Wu <adaptee@gmail.com>
2012-06-24 12:31:41 +0800

    Do not show context menu when right click in midnight commander
    BUG: 302427
    FIXED-IN: 4.9.0

--- src/TerminalDisplay.cpp
+++ src/TerminalDisplay.cpp
@@ -2795,12 +2795,15 @@ bool TerminalDisplay::event(QEvent* event)
     return eventHandled ? true : QWidget::event(event);
 }
 
-void TerminalDisplay::contextMenuEvent(QContextMenuEvent*)
+void TerminalDisplay::contextMenuEvent(QContextMenuEvent* event)
 {
 // FIXME: On Mac OSX opening some dialogs cause the context menu to
 //  get displayed.  Example: Scrollback dialog via context menu.
 #if !defined(Q_OS_MAC)
-    emit configureRequest(mapFromGlobal(QCursor::pos()));
+    // the logic for the mouse case is within MousePressEvent()
+    if (event->reason() != QContextMenuEvent::Mouse) {
+        emit configureRequest(mapFromGlobal(QCursor::pos()));
+    }
 #endif
 }
 
