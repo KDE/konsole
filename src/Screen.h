7937cb979369a033a54bbb27dc963565b49b845b
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2013-05-17 09:12:29 -0400

    Add boundary check in copyLineToStream to try to prevent crashes
    A lot of crashes in copyLineToStream _screenLines[screenLine]; this
    likely needs to be a container, for now add an extra variable to watch
    the size.
    I still can't reproduce it though a lot of people can doing various
    things.
    319888, 313711, 312006, 306939, 304837

--- src/Screen.h
+++ src/Screen.h
@@ -649,6 +649,7 @@ private:
 
     typedef QVector<Character> ImageLine;      // [0..columns]
     ImageLine*          _screenLines;    // [lines]
+    int _screenLinesSize;                // _screenLines.size()
 
     int _scrolledLines;
     QRect _lastScrolledRegion;
