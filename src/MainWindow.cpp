0a5e2b34971e339561e60c674a6fa5ec7eb941da
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2016-05-21 18:43:10 -0400

    When setting window size, take into account when menubar is not visible
    Take into account menubar visibility when sizing a new window.
    Note that this is only used when 'Use current window size on next
    startup' is unchecked.
    
    Thanks to Roman Gilg subdiff gmail com for patch
    
    BUG: 359662
    REVIEW: 27803

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -823,6 +823,8 @@ void MainWindow::showEvent(QShowEvent* aEvent)
         menuBar()->setVisible(_menuBarInitialVisibility);
         _toggleMenuBarAction->setChecked(_menuBarInitialVisibility);
         _menuBarInitialVisibilityApplied = true;
+        if(!KonsoleSettings::saveGeometryOnExit())
+            resize(sizeHint());
     }
 
     // Call parent method
