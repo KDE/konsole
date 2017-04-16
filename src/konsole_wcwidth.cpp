fef096b51aa91c8a0bb0ee8ec77789f84c1d7e1d
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2017-04-16 18:39:57 -0400

    reduce variable scope

--- src/konsole_wcwidth.cpp
+++ src/konsole_wcwidth.cpp
@@ -79,12 +79,11 @@ struct interval {
 static int bisearch(unsigned long ucs, const struct interval* table, int max)
 {
     int min = 0;
-    int mid;
 
     if (ucs < table[0].first || ucs > table[max].last)
         return 0;
     while (max >= min) {
-        mid = (min + max) / 2;
+        const int mid = (min + max) / 2;
         if (ucs > table[mid].last)
             min = mid + 1;
         else if (ucs < table[mid].first)
