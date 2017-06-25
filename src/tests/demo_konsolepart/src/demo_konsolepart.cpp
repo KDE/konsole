f1e991a6c568adf2ae59350675ed8d682f64bf57
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2017-06-25 17:28:15 -0400

    Add a test/demo app for KonsolePart
    binary is not installed; can be used to test ManageProfile dialog ATM

---
+++ src/tests/demo_konsolepart/src/demo_konsolepart.cpp
@@ -0,0 +1,82 @@
+/*
+ Copyright 2017 by Kurt Hindenburg <kurt.hindenburg@gmail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation; either version 2 of
+ the License or (at your option) version 3 or any later version
+ accepted by the membership of KDE e.V. (or its successor approved
+ by the membership of KDE e.V.), which shall act as a proxy 
+ defined in Section 14 of version 3 of the license.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "demo_konsolepart.h"
+
+#include <KPluginLoader>
+#include <KPluginFactory>
+#include <KService>
+#include <KStandardAction>
+
+#include <QApplication>
+#include <QMenu>
+#include <QMenuBar>
+
+demo_konsolepart::demo_konsolepart()
+    : KMainWindow()
+{
+    // Create terminal part and embed in into the main window
+    _terminalPart = createPart();
+    if (!_terminalPart) {
+        return;
+    }
+
+    QMenu* fileMenu = menuBar()->addMenu(QStringLiteral("File"));
+    QAction* manageProfilesAction = fileMenu->addAction(QStringLiteral("Manage Profiles..."));
+    connect(manageProfilesAction, &QAction::triggered, this, &demo_konsolepart::manageProfiles);
+    QAction* quitAction = fileMenu->addAction(QStringLiteral("Quit"));
+    connect(quitAction, &QAction::triggered, this, &demo_konsolepart::quit);
+
+    connect(_terminalPart, &KParts::ReadOnlyPart::destroyed, this, &demo_konsolepart::quit);
+
+    setCentralWidget(_terminalPart->widget());
+    _terminal = qobject_cast<TerminalInterface*>(_terminalPart);
+
+}
+
+demo_konsolepart::~demo_konsolepart()
+{
+    if (_terminalPart) {
+        disconnect(_terminalPart, &KParts::ReadOnlyPart::destroyed, this, &demo_konsolepart::quit);
+    }
+}
+
+KParts::ReadOnlyPart* demo_konsolepart::createPart()
+{
+    KService::Ptr service = KService::serviceByDesktopName(QStringLiteral("konsolepart"));
+    Q_ASSERT(service);
+    KPluginFactory* factory = KPluginLoader(service->library()).factory();
+    Q_ASSERT(factory);
+
+    KParts::ReadOnlyPart* terminalPart = factory->create<KParts::ReadOnlyPart>(this);
+
+    return terminalPart;
+}
+
+void demo_konsolepart::manageProfiles()
+{
+    QMetaObject::invokeMethod(_terminalPart, "showManageProfilesDialog",
+        Qt::QueuedConnection, Q_ARG(QWidget*, QApplication::activeWindow()));
+}
+
+void demo_konsolepart::quit()
+{
+    close();
+}
---
+++ src/tests/demo_konsolepart/src/main.cpp
@@ -0,0 +1,66 @@
+/*
+ Copyright 2017 by Kurt Hindenburg <kurt.hindenburg@gmail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as
+ published by the Free Software Foundation; either version 2 of
+ the License or (at your option) version 3 or any later version
+ accepted by the membership of KDE e.V. (or its successor approved
+ by the membership of KDE e.V.), which shall act as a proxy 
+ defined in Section 14 of version 3 of the license.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// application header
+#include "demo_konsolepart.h"
+
+// KF headers
+#include <KAboutData>
+#include <KLocalizedString>
+
+// Qt headers
+#include <QApplication>
+#include <QCommandLineParser>
+
+int main(int argc, char **argv)
+{
+    auto app = new QApplication(argc, argv);
+
+    KLocalizedString::setApplicationDomain("demo_konsolepart");
+
+    KAboutData about(QStringLiteral("demo_konsolepart"),
+                     i18nc("@title", "Demo KonsolePart"),
+                     QStringLiteral("1.0"),
+                     i18nc("@title", "Terminal emulator"),
+                     KAboutLicense::GPL_V2,
+                     i18nc("@info:credit", "(c) 2017, The Konsole Developers"),
+                     QStringLiteral(),
+                     QStringLiteral("https://konsole.kde.org/"));
+
+    KAboutData::setApplicationData(about);
+
+    QSharedPointer<QCommandLineParser> parser(new QCommandLineParser);
+    parser->setApplicationDescription(about.shortDescription());
+    parser->addHelpOption();
+    parser->addVersionOption();
+    about.setupCommandLine(parser.data());
+
+    QStringList args = QStringList(app->arguments());
+
+    parser->process(args);
+    about.processCommandLine(parser.data());
+
+    demo_konsolepart *demo = new demo_konsolepart();
+    demo->show();
+
+    int ret = app->exec();
+    delete app;
+    return ret;
+}
