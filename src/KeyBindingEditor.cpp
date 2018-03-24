fefd2e56c0875ece23e90de4b77d8e09c3c5577d
Ahmad Samir <a.samirh78@gmail.com>
2018-03-24 12:14:21 -0400

    Make the key binding editor dialog bigger
    Summary:
    The first column in the key binding editor dialog usually has long lines of
    text, to improve usability set a minimum size of 480x430 for that dialog
    and an initial size of 500x500. Also set the initial width of that column
    to 300.
    
    Reviewers: #konsole, hindenburg
    
    Reviewed By: #konsole, hindenburg
    
    Subscribers: ngraham, #konsole
    
    Tags: #konsole
    
    Differential Revision: https://phabricator.kde.org/D11637
    
    (cherry picked from commit bd560c563f16a9d14473607f09dfe7c3a6c490ee)

--- src/KeyBindingEditor.cpp
+++ src/KeyBindingEditor.cpp
@@ -52,6 +52,10 @@ KeyBindingEditor::KeyBindingEditor(QWidget *parent) :
 
     _ui->keyBindingTable->setHorizontalHeaderLabels(labels);
     _ui->keyBindingTable->horizontalHeader()->setStretchLastSection(true);
+
+    // see also the sizes set in EditProfileDialog::showKeyBindingEditor()
+    _ui->keyBindingTable->setColumnWidth(0, 300);
+
     _ui->keyBindingTable->verticalHeader()->hide();
     _ui->keyBindingTable->setSelectionBehavior(QAbstractItemView::SelectRows);
 
