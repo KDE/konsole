975831314a6c09cebb97ad03ccace92e33bea7b0
Ovidiu-Florin Bogdan <ovidiu-florin.bogdan@intel.com>
2018-07-22 09:49:53 +0300

    update conanfile for building an executable

--- conanfile.py
+++ conanfile.py
@@ -4,24 +4,18 @@ from conans import ConanFile, CMake
 class KonsoleConan(ConanFile):
     name = "konsole"
     version = "18.11.70"
-    license = "<Put the package license here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Konsole here>"
+    license = "GPLv2"
+    url = "https://www.kde.org/applications/system/konsole/"
+    description = "Konsole is the terminal emulator"
+
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+
     generators = "cmake"
-    exports_sources = "src/*"
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="src")
+        cmake.configure()
         cmake.build()
-
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
         self.copy("*.h", dst="include", src="src")
@@ -30,6 +24,3 @@ class KonsoleConan(ConanFile):
         self.copy("*.dylib*", dst="lib", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
-
-    def package_info(self):
-        self.cpp_info.libs = ["hello"]
