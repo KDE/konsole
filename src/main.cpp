KDE/konsole

commit ac59cc7e007a3ef73a07f3d31d4a9516fd5f56f5

Fix hang on a lot of output from a program
Summary:
There is a bug in the Qt glib event loop leading to timers never being
able to deliver signals.

Work around this by disabling the glib event loop.

References:
http://lists.qt-project.org/pipermail/interest/2015-September/018846.html
https://bugreports.qt.io/browse/QTBUG-48344

Test plan:
From the referenced bug:
    Stefan Westerfeld 2010-03-10 11:40:24 UTC
    Running the following program within konsole:

        #include <stdio.h>

        int
        main()
        {
          for (int i = 0; i < 100000000; i++)
            {
              fprintf (stderr, "foo %d\n", i);
            }
        }

    leads to a freeze - not single message is printed - no reaction on
    return. Only after a long time (30 seconds) something happens.

    If I run the same program in an xterm, the messages are scrolling
    through, as I would expect from that kind of output.

Reviewers: #konsole, hindenburg

Reviewed By: #konsole, hindenburg

Subscribers: hindenburg

Tags: #konsole

Differential Revision: https://phabricator.kde.org/D6078

BUG: 230184

src/main.cpp
@@ -75,6 +75,14 @@ extern "C" int Q_DECL_EXPORT kdemain(int argc, char *argv[])
         needToDeleteQApplication = true;
     }
 
+#if defined(Q_OS_LINUX)
+    // Workaround for https://bugreports.qt.io/browse/QTBUG-48344
+    // See also https://bugs.kde.org/show_bug.cgi?id=230184
+    // The Qt glib event loop doesn't let timers deliver events if there are a
+    // lot of other events.
+    qputenv("QT_NO_GLIB", "1");
+#endif
+
     auto app = new QApplication(argc, argv);
 
     // enable high dpi support

tests/spam-stderr.c
@@ -1,0 +1,15 @@
+// from https://bugs.kde.org/show_bug.cgi?id=230184
+// author Stefan Westerfeld
+
+#include <stdio.h>
+
+int
+main()
+{
+  for (int i = 0; i < 100000000; i++)
+    {
+      fprintf (stderr, "foo %d\n", i);
+    }
+}
+
+

commit 76b8b8d71d17a35f0537035a50e81904879de586

Don't pollute the environment inside Konsole with QT_NO_GLIB
Summary:
Fixes a regression from ac59cc7e007a3ef73a07.
LibreOffice Writer, for example, will hang during startup when
QT_NO_GLIB is set. It uses GLib and has KDE integration.

Test Plan:
In a shell, set QT_NO_GLIB to a string, or an empty string, or
unset it. Run konsole from that shell, check that QT_NO_GLIB in
the new shell inside konsole has the same value as in the
launching shell.

Reviewers: hindenburg, #konsole, sandsmark, dfaure

Reviewed By: dfaure

Subscribers: dfaure, konsole-devel

Tags: #konsole

Differential Revision: https://phabricator.kde.org/D14558

src/main.cpp
@@ -80,10 +80,19 @@ extern "C" int Q_DECL_EXPORT kdemain(int argc, char *argv[])
     // See also https://bugs.kde.org/show_bug.cgi?id=230184
     // The Qt glib event loop doesn't let timers deliver events if there are a
     // lot of other events.
+    const QByteArray qtUseGLibOld = qgetenv("QT_NO_GLIB");
     qputenv("QT_NO_GLIB", "1");
 #endif
 
     auto app = new QApplication(argc, argv);
+
+#if defined(Q_OS_LINUX)
+    if (qtUseGLibOld.isNull()) {
+        qunsetenv("QT_NO_GLIB");
+    } else {
+        qputenv("QT_NO_GLIB", qtUseGLibOld);
+    }
+#endif
 
     // enable high dpi support
     app->setAttribute(Qt::AA_UseHighDpiPixmaps, true);