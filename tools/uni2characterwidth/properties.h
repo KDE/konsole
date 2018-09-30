5f32cb3c44f2b1cb3faa2efd2371da4af80dce2e
Mariusz Glebocki <mglb@arccos-1.net>
2018-09-30 11:36:43 -0400

    Add a tool for generating character width tables
    Summary:
    The uni2characterwidth tool, converts Unicode Character Database files
    into character width lookup tables. It uses a template file to place
    the tables in a source code file together with a function for finding
    the width for specified character. It also allows to generate few forms
    of lists with width data for debug and test purposes, or for future use
    as a replacement of Unicode files.
    
    Set `KONSOLE_BUILD_UNI2CHARACTERWIDTH` cmake flag to build the tool.
    Use `--help` argument for more detailed usage.
    
    There is a possibility to generate separate "width" for Ambiguous
    characters. It can be used to add ability to configure the characters
    width in Konsole settings.
    
    The `example.template` file contains all possible named tags, and some
    additional tags to show how to use them.
    
    CCBUG: 396435
    
    Depends on D15756
    
    Test Plan:
    Download files listed below from `11.0.0` and `emoji/11.0` directories
    on `https://unicode.org/Public/`. You can also directly use URLs to the
    files.
    
    * UnicodeData.txt
    * EastAsianWidth.txt
    * emoji-data.txt
    
    Generate any available list except compact-ranges (e.g. `details`):
    
    ```
    uni2characterwidth \
        -U UnicodeData.txt  -A EastAsianWidth.txt  -E emoji-data.txt \
        -g details  result.txt
    ```
    
    The list should contain ranges for all possible widths
    (-2, -1, 0, 1, 2). You can choose some characters with a width you know
    and check how they were classified. -2 is a special non-standard width
    for ambiguous characters, which can be overriden by adding `-a 1` or
    `-a 2` parameter. With this flag, all ranges from -2 group should
    disappear and become assigned to selected width (1 or 2).
    
    Generate output using a template:
    
    ```
    uni2characterwidth \
        -U UnicodeData.txt  -A EastAsianWidth.txt  -E emoji-data.txt \
        -g code,./template.example  result.txt
    ```
    
    Reviewers: #konsole, hindenburg
    
    Reviewed By: #konsole, hindenburg
    
    Subscribers: hindenburg, konsole-devel
    
    Tags: #konsole
    
    Differential Revision: https://phabricator.kde.org/D15757

---
+++ tools/uni2characterwidth/properties.h
@@ -0,0 +1,78 @@
+#ifndef CATEGORY_PROPERTY_VALUE
+#define CATEGORY_PROPERTY_VALUE(val, sym, intVal)
+#endif
+#ifndef CATEGORY_PROPERTY_GROUP
+#define CATEGORY_PROPERTY_GROUP(val, sym, intVal)
+#endif
+
+CATEGORY_PROPERTY_VALUE(Lu, UppercaseLetter,        1<<0)   // an uppercase letter
+CATEGORY_PROPERTY_VALUE(Ll, LowercaseLetter,        1<<1)   // a lowercase letter
+CATEGORY_PROPERTY_VALUE(Lt, TitlecaseLetter,        1<<2)   // a digraphic character, with first part uppercase
+CATEGORY_PROPERTY_GROUP(LC, CasedLetter,            1<<0|1<<1|1<<2)
+CATEGORY_PROPERTY_VALUE(Lm, ModifierLetter,         1<<3)   // a modifier letter
+CATEGORY_PROPERTY_VALUE(Lo, OtherLetter,            1<<4)   // other letters, including syllables and ideographs
+CATEGORY_PROPERTY_GROUP(L,  Letter,                 1<<0|1<<1|1<<2|1<<3|1<<4)
+CATEGORY_PROPERTY_VALUE(Mn, NonspacingMark,         1<<5)   // a nonspacing combining mark (zero advance width)
+CATEGORY_PROPERTY_VALUE(Mc, SpacingMark,            1<<6)   // a spacing combining mark (positive advance width)
+CATEGORY_PROPERTY_VALUE(Me, EnclosingMark,          1<<7)   // an enclosing combining mark
+CATEGORY_PROPERTY_GROUP(M,  Mark,                   1<<5|1<<6|1<<7)
+CATEGORY_PROPERTY_VALUE(Nd, DecimalNumber,          1<<8)   // a decimal digit
+CATEGORY_PROPERTY_VALUE(Nl, LetterNumber,           1<<9)   // a letterlike numeric character
+CATEGORY_PROPERTY_VALUE(No, OtherNumber,            1<<10)  // a numeric character of other type
+CATEGORY_PROPERTY_GROUP(N,  Number,                 1<<8|1<<9|1<<10)
+CATEGORY_PROPERTY_VALUE(Pc, ConnectorPunctuation,   1<<11)  // a connecting punctuation mark, like a tie
+CATEGORY_PROPERTY_VALUE(Pd, DashPunctuation,        1<<12)  // a dash or hyphen punctuation mark
+CATEGORY_PROPERTY_VALUE(Ps, OpenPunctuation,        1<<13)  // an opening punctuation mark (of a pair)
+CATEGORY_PROPERTY_VALUE(Pe, ClosePunctuation,       1<<14)  // a closing punctuation mark (of a pair)
+CATEGORY_PROPERTY_VALUE(Pi, InitialPunctuation,     1<<15)  // an initial quotation mark
+CATEGORY_PROPERTY_VALUE(Pf, FinalPunctuation,       1<<16)  // a final quotation mark
+CATEGORY_PROPERTY_VALUE(Po, OtherPunctuation,       1<<17)  // a punctuation mark of other type
+CATEGORY_PROPERTY_GROUP(P,  Punctuation,            1<<11|1<<12|1<<13|1<<14|1<<15|1<<16|1<<17)
+CATEGORY_PROPERTY_VALUE(Sm, MathSymbol,             1<<18)  // a symbol of mathematical use
+CATEGORY_PROPERTY_VALUE(Sc, CurrencySymbol,         1<<19)  // a currency sign
+CATEGORY_PROPERTY_VALUE(Sk, ModifierSymbol,         1<<20)  // a non-letterlike modifier symbol
+CATEGORY_PROPERTY_VALUE(So, OtherSymbol,            1<<21)  // a symbol of other type
+CATEGORY_PROPERTY_GROUP(S,  Symbol,                 1<<18|1<<19|1<<20|1<<21)
+CATEGORY_PROPERTY_VALUE(Zs, SpaceSeparator,         1<<22)  // a space character (of various non-zero widths)
+CATEGORY_PROPERTY_VALUE(Zl, LineSeparator,          1<<23)  // U+2028 LINE SEPARATOR only
+CATEGORY_PROPERTY_VALUE(Zp, ParagraphSeparator,     1<<24)  // U+2029 PARAGRAPH SEPARATOR only
+CATEGORY_PROPERTY_GROUP(Z,  Separator,              1<<22|1<<23|1<<24)
+CATEGORY_PROPERTY_VALUE(Cc, Control,                1<<25)  // a C0 or C1 control code
+CATEGORY_PROPERTY_VALUE(Cf, Format,                 1<<26)  // a format control character
+CATEGORY_PROPERTY_VALUE(Cs, Surrogate,              1<<27)  // a surrogate code point
+CATEGORY_PROPERTY_VALUE(Co, PrivateUse,             1<<28)  // a private-use character
+CATEGORY_PROPERTY_VALUE(Cn, Unassigned,             1<<29)  // a reserved unassigned code point or a noncharacter
+CATEGORY_PROPERTY_GROUP(C,  Other,                  1<<25|1<<26|1<<27|1<<28|1<<29)
+
+#undef CATEGORY_PROPERTY_VALUE
+#undef CATEGORY_PROPERTY_GROUP
+
+/**************************************/
+
+#ifndef EAST_ASIAN_WIDTH_PROPERTY_VALUE
+#define EAST_ASIAN_WIDTH_PROPERTY_VALUE(val, sym, intVal)
+#endif
+
+EAST_ASIAN_WIDTH_PROPERTY_VALUE(A,  Ambiguous,  1<<0)
+EAST_ASIAN_WIDTH_PROPERTY_VALUE(F,  Fullwidth,  1<<1)
+EAST_ASIAN_WIDTH_PROPERTY_VALUE(H,  Halfwidth,  1<<2)
+EAST_ASIAN_WIDTH_PROPERTY_VALUE(N,  Neutral,    1<<3)
+EAST_ASIAN_WIDTH_PROPERTY_VALUE(Na, Narrow,     1<<4)
+EAST_ASIAN_WIDTH_PROPERTY_VALUE(W,  Wide,       1<<5)
+
+#undef EAST_ASIAN_WIDTH_PROPERTY_VALUE
+
+/**************************************/
+
+#ifndef EMOJI_PROPERTY_VALUE
+#define EMOJI_PROPERTY_VALUE(val, sym, intVal)
+#endif
+
+EMOJI_PROPERTY_VALUE(,                      None,               0)
+EMOJI_PROPERTY_VALUE(Emoji,                 Emoji,              1<<0)
+EMOJI_PROPERTY_VALUE(Emoji_Presentation,    EmojiPresentation,  1<<1)
+EMOJI_PROPERTY_VALUE(Emoji_Modifier,        EmojiModifier,      1<<2)
+EMOJI_PROPERTY_VALUE(Emoji_Modifier_Base,   EmojiModifier_Base, 1<<3)
+EMOJI_PROPERTY_VALUE(Emoji_Component,       EmojiComponent,     1<<4)
+
+#undef EMOJI_PROPERTY_VALUE
---
+++ tools/uni2characterwidth/template.h
@@ -0,0 +1,184 @@
+/*
+    This file is part of Konsole, a terminal emulator for KDE.
+
+    Copyright 2018 by Mariusz Glebocki <mglb@arccos-1.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301  USA.
+*/
+
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+#include <QMap>
+#include <QString>
+#include <QVector>
+
+// Backward compatibility
+#if QT_VERSION < QT_VERSION_CHECK(5, 7, 0) && !defined(qAsConst)
+#define qAsConst(code) code
+#endif
+
+// QVariant doesn't offer modification in place. Var does.
+class Var {
+public:
+    using Number    = qint64;
+    using String    = QString;
+    using Map       = QMap<String, Var>;
+    using Vector    = QVector<Var>;
+
+    enum class DataType {
+        Invalid,
+        Number,
+        String,
+        Vector,
+        Map,
+    };
+
+    const QString dataTypeAsString() const {
+        switch(dataType()) {
+            case DataType::Invalid: return QStringLiteral("Invalid");
+            case DataType::Number:  return QStringLiteral("Number");
+            case DataType::String:  return QStringLiteral("String");
+            case DataType::Vector:  return QStringLiteral("Vector");
+            case DataType::Map:     return QStringLiteral("Map");
+            default:                return QStringLiteral("Unknown?");
+        }
+    }
+
+    Var(): num(0), _dataType(DataType::Invalid) {}
+    Var(const Var &other) { *this = other; }
+
+    Var(const Number &newNum): _dataType(DataType::Number) { new(&num) auto(newNum); }
+    Var(const String &newStr): _dataType(DataType::String) { new(&str) auto(newStr); }
+    Var(const Vector &newVec): _dataType(DataType::Vector) { new(&vec) auto(newVec); }
+    Var(const Map    &newMap): _dataType(DataType::Map)    { new(&map) auto(newMap); }
+
+    // Allow initialization without type name
+    Var(const char *               newStr): _dataType(DataType::String) { new(&str) String(QString::fromUtf8(newStr)); }
+    Var(std::initializer_list<Var> newVec): _dataType(DataType::Vector) { new(&vec) Vector(newVec); }
+
+    ~Var() {
+        switch(dataType()) {
+            case DataType::String: str.~String(); break;
+            case DataType::Vector: vec.~Vector(); break;
+            case DataType::Map:    map.~Map();    break;
+            default: break;
+        }
+    }
+
+    Var & operator=(const Var &other) {
+        _dataType = other.dataType();
+        switch(other.dataType()) {
+            case DataType::Number: new(&num) auto(other.num); break;
+            case DataType::String: new(&str) auto(other.str); break;
+            case DataType::Vector: new(&vec) auto(other.vec); break;
+            case DataType::Map:    new(&map) auto(other.map); break;
+            default: break;
+        }
+        return *this;
+    }
+
+    Var & operator[](unsigned index) {
+        Q_ASSERT(_dataType == DataType::Vector);
+        return vec.data()[index];
+    }
+    const Var & operator[](unsigned index) const {
+        Q_ASSERT(_dataType == DataType::Vector);
+        return vec.constData()[index];
+    }
+    Var & operator[](const String &key) {
+        Q_ASSERT(_dataType == DataType::Map);
+        return map[key];
+    }
+    const Var & operator[](const String &key) const {
+        Q_ASSERT(_dataType == DataType::Map);
+        return *map.find(key);
+    }
+
+    DataType dataType() const { return _dataType; }
+
+    union {
+        Number  num;
+        String  str;
+        Vector  vec;
+        Map     map;
+    };
+
+private:
+    DataType _dataType;
+};
+
+class Template {
+public:
+    Template(const QString &text);
+    void parse();
+    QString generate(const Var &data);
+
+    struct Element {
+        Element(const Element *parent = nullptr, const QString &name = QString())
+                : outer()
+                , inner()
+                , name(name)
+                , fmt()
+                , line(0)
+                , column(0)
+                , isComment(false)
+                , children()
+                , parent(parent) {}
+
+        Element(const Element &other)
+                : outer(other.outer)
+                , inner(other.inner)
+                , name(other.name)
+                , fmt(other.fmt)
+                , line(other.line)
+                , column(other.column)
+                , isComment(other.isComment)
+                , parent(other.parent) {
+            for(const auto &child: other.children) {
+                children.append(child);
+            }
+        }
+
+        const QString findFmt(Var::DataType type) const;
+        QString path() const;
+        bool isCommand() const { return name.startsWith(QLatin1Char('!')); }
+        bool hasName() const { return !isCommand() && !name.isEmpty(); }
+
+        static const QString defaultFmt(Var::DataType type);
+        static bool isValidFmt(const QString &fmt, Var::DataType type);
+
+        QStringRef outer;
+        QStringRef inner;
+        QString name;
+        QString fmt;
+        uint line;
+        uint column;
+        bool isComment;
+        QList<Element> children;
+        const Element *parent;
+    };
+private:
+
+    void executeCommand(Element &element, const Element &childStub, const QStringList &argv);
+    void parseRecursively(Element &element);
+    int generateRecursively(QString &result, const Element &element, const Var &data, int consumed = 0);
+
+    QString _text; // FIXME: make it pointer (?)
+    Element _root; // FIXME: make it pointer
+};
+
+#endif
