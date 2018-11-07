KDE/konsole

commit f93263d6b4dca25416b17c96ac9a996ee2059ef6

[Terminal Display] Hide search bar on ESC
Differential Revision: https://phabricator.kde.org/D15572

Signed-off-by: Anthony Fieroni <bvbfan@abv.bg>

src/TerminalDisplay.cpp
@@ -3595,6 +3595,10 @@ void TerminalDisplay::keyPressEvent(QKeyEvent* event)
         }
     }
 
+    if (_searchBar->isVisible() && (event->key() & Qt::Key_Escape)) {
+        _searchBar->hide();
+    }
+
     emit keyPressedSignal(event);
 
 #ifndef QT_NO_ACCESSIBILITY

commit 1c4f6818d6761f4f2f567d957a777f964bc41154

[Terminal Display] Don't hide the search box when scrolling with the keyboard
Summary:
When hitting Shift-Scrollup while having the search box opened, the search
box is closed.

Regression introduced by f93263d6b4dca25416b17c96ac9a996ee2059ef6

Test Plan:
Open the search box, then scroll with the keyboard (e.g. with Shift-Scrollup)
The searchbox should not close.

Reviewers: #konsole, anthonyfieroni

Reviewed By: anthonyfieroni

Subscribers: ngraham, anthonyfieroni, konsole-devel

Tags: #konsole

Differential Revision: https://phabricator.kde.org/D16714

src/TerminalDisplay.cpp
@@ -3587,7 +3587,7 @@ void TerminalDisplay::keyPressEvent(QKeyEvent* event)
         }
     }
 
-    if (_searchBar->isVisible() && (event->key() & Qt::Key_Escape)) {
+    if (_searchBar->isVisible() && event->key() == Qt::Key_Escape) {
         _searchBar->hide();
     }
 