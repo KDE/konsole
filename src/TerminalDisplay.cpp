KDE/konsole

commit dd03c26ad1a7a59c12434a0934c7ff1541a259ba

set variable in initialize list and explictly use bool in if

src/TerminalDisplay.cpp
@@ -479,6 +479,7 @@ TerminalDisplay::TerminalDisplay(QWidget* parent)
     , _dimWhenInactive(false)
     , _scrollWheelState(ScrollState())
     , _searchBar(new IncrementalSearchBar(this))
+    , _searchResultRect(QRect())
 {
     // terminal applications are not designed with Right-To-Left in mind,
     // so the layout is forced to Left-To-Right
@@ -1432,7 +1433,7 @@ void TerminalDisplay::updateImage()
 
     dirtyRegion |= _inputMethodData.previousPreeditRect;
 
-    if ((_screenWindow->currentResultLine() != -1) && (_screenWindow->scrollCount())) {
+    if ((_screenWindow->currentResultLine() != -1) && (_screenWindow->scrollCount() > 0)) {
         // De-highlight previous result region
         dirtyRegion |= _searchResultRect;
         // Highlight new result region

commit f193805dde5908bfb60bfd4724965a50c9910495

Fix condition to redraw search result line
Summary: Regression from dd03c26ad1a7a59c12434a0934c7ff1541a259ba

Test Plan: Same than D16820

Reviewers: #konsole, hindenburg

Reviewed By: #konsole, hindenburg

Subscribers: hindenburg, konsole-devel

Tags: #konsole

Differential Revision: https://phabricator.kde.org/D17471

src/TerminalDisplay.cpp
@@ -1557,7 +1557,7 @@ void TerminalDisplay::updateImage()
 
     dirtyRegion |= _inputMethodData.previousPreeditRect;
 
-    if ((_screenWindow->currentResultLine() != -1) && (_screenWindow->scrollCount() > 0)) {
+    if ((_screenWindow->currentResultLine() != -1) && (_screenWindow->scrollCount() != 0)) {
         // De-highlight previous result region
         dirtyRegion |= _searchResultRect;
         // Highlight new result region