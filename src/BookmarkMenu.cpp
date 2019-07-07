12d23264c41f4c84b242847c1fce9dfc40c144bc
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2019-07-07 13:04:51 +0200

    forgot copyright/license headers

--- src/BookmarkMenu.cpp
+++ src/BookmarkMenu.cpp
@@ -1,6 +1,26 @@
+/*  This file was part of the KDE libraries
+
+    Copyright 2019 by Tomaz Canabrava <tcanabrava@kde.org>
+    Copyright 2019 by Martin Sandsmark <martin.sandsmark@kde.org>
+
+    library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation, version 2
+    or ( at your option ), any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
 
 // Own
-#include <BookmarkMenu.h>
+#include "BookmarkMenu.h"
 
 // KDE
 #include <KActionCollection>
