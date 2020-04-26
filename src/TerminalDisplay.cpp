caeca72a01442e60ba4257fc1bb9c55bb107bf70
Ahmad Samir <a.samirh78@gmail.com>
2020-04-26 14:50:03 -0400

    Change depreciated orientation() to angleDelta()

--- src/TerminalDisplay.cpp
+++ src/TerminalDisplay.cpp
@@ -2700,7 +2700,7 @@ void TerminalDisplay::mouseDoubleClickEvent(QMouseEvent* ev)
 void TerminalDisplay::wheelEvent(QWheelEvent* ev)
 {
     // Only vertical scrolling is supported
-    if (ev->orientation() != Qt::Vertical) {
+    if (ev->angleDelta().x() != 0) {
         return;
     }
 
