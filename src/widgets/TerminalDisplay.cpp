23024d4b0adda7d5b77560565f6163c285429816
Gabriele Mazzotta <gabriele.mzt@gmail.com>
2020-09-05 10:38:44 -0400

    Fix vertical scrolling with touchpads and trackpoints.
    In case of diagonal scrolling, which can happen when using touchpads,
    'x' is not zero.
    
    BUG: 425765
    FIXED-IN: 20.08.2

--- src/widgets/TerminalDisplay.cpp
+++ src/widgets/TerminalDisplay.cpp
@@ -2850,7 +2850,7 @@ void TerminalDisplay::mouseDoubleClickEvent(QMouseEvent* ev)
 void TerminalDisplay::wheelEvent(QWheelEvent* ev)
 {
     // Only vertical scrolling is supported
-    if (ev->angleDelta().x() != 0) {
+    if (qAbs(ev->angleDelta().y()) < qAbs(ev->angleDelta().x())) {
         return;
     }
 
