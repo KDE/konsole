b6e8517756ebe95e5117a4a4b55f01016ae0380e
Gustavo Carneiro <gcarneiroa@hotmail.com>
2020-10-02 18:09:13 -0300

    Remove function prototypes.

--- src/terminalDisplay/TerminalDisplay.h
+++ src/terminalDisplay/TerminalDisplay.h
@@ -656,50 +656,6 @@ private Q_SLOTS:
 private:
     Q_DISABLE_COPY(TerminalDisplay)
 
-    // -- Drawing helpers --
-
-    // divides the part of the display specified by 'rect' into
-    // fragments according to their colors and styles and calls
-    // drawTextFragment() or drawPrinterFriendlyTextFragment()
-    // to draw the fragments
-    void drawContents(QPainter &painter, const QRect &rect);
-    // draw a transparent rectangle over the line of the current match
-    void drawCurrentResultRect(QPainter &painter);
-    // draw a thin highlight on the left of the screen for lines that have been scrolled into view
-    void highlightScrolledLines(QPainter& painter);
-    // compute which region need to be repainted for scrolled lines highlight
-    QRegion highlightScrolledLinesRegion(bool nothingChanged);
-
-    // draws a section of text, all the text in this section
-    // has a common color and style
-    void drawTextFragment(QPainter &painter, const QRect &rect, const QString &text,
-                          const Character *style);
-
-    void drawPrinterFriendlyTextFragment(QPainter &painter, const QRect &rect, const QString &text,
-                                         const Character *style);
-    // draws the background for a text fragment
-    // if useOpacitySetting is true then the color's alpha value will be set to
-    // the display's transparency (set with setOpacity()), otherwise the background
-    // will be drawn fully opaque
-    void drawBackground(QPainter &painter, const QRect &rect, const QColor &backgroundColor,
-                        bool useOpacitySetting);
-    // draws the cursor character
-    void drawCursor(QPainter &painter, const QRect &rect, const QColor &foregroundColor,
-                    const QColor &backgroundColor, QColor &characterColor);
-    // draws the characters or line graphics in a text fragment
-    void drawCharacters(QPainter &painter, const QRect &rect, const QString &text,
-                        const Character *style, const QColor &characterColor);
-    // draws a string of line graphics
-    void drawLineCharString(QPainter &painter, int x, int y, const QString &str,
-                            const Character *attributes);
-
-    // draws the preedit string for input methods
-    void drawInputMethodPreeditString(QPainter &painter, const QRect &rect);
-
-    // --
-
-
-
     // the area where the preedit string for input methods will be draw
     QRect preeditRect() const;
 
