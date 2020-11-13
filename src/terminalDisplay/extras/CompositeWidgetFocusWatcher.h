336a51077b59105096bb0a624c965587225a82b3
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2020-11-12 21:41:36 -0500

    Convert terminalDisplay to use SPDX license/copyright
    https://community.kde.org/Policies/Licensing_Policy#SPDX_Statements
    https://community.kde.org/Guidelines_and_HOWTOs/Licensing

--- src/terminalDisplay/extras/CompositeWidgetFocusWatcher.h
+++ src/terminalDisplay/extras/CompositeWidgetFocusWatcher.h
@@ -1,23 +1,10 @@
 /*
     This file is part of Konsole, a terminal emulator for KDE.
 
-    Copyright 2006-2008 by Robert Knight <robertknight@gmail.com>
-    Copyright 1997,1998 by Lars Doelle <lars.doelle@on-line.de>
+    SPDX-FileCopyrightText: 2006-2008 Robert Knight <robertknight@gmail.com>
+    SPDX-FileCopyrightText: 1997, 1998 Lars Doelle <lars.doelle@on-line.de>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-    02110-1301  USA.
+    SPDX-License-Identifier: GPL-2.0-or-later
 */
 
 #ifndef COMPOSITEWIDGET_H
