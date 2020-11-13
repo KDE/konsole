78a0092522c3d3a32413ce0e4b485716bb21fe0f
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2020-11-12 22:17:06 -0500

    Convert src to use SPDX license/copyright
    https://community.kde.org/Policies/Licensing_Policy#SPDX_Statements
    https://community.kde.org/Guidelines_and_HOWTOs/Licensing

--- src/BookmarkHandler.cpp
+++ src/BookmarkHandler.cpp
@@ -1,22 +1,9 @@
 /*  This file was part of the KDE libraries
 
-    Copyright 2002 Carsten Pfeiffer <pfeiffer@kde.org>
-    Copyright 2007-2008 Robert Knight <robertknight@gmail.com>
+    SPDX-FileCopyrightText: 2002 Carsten Pfeiffer <pfeiffer@kde.org>
+    SPDX-FileCopyrightText: 2007-2008 Robert Knight <robertknight@gmail.com>
 
-    library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation, version 2
-    or ( at your option ), any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+    SPDX-License-Identifier: GPL-2.0-or-later
 */
 
 // Born as kdelibs/kio/kfile/kfilebookmarkhandler.cpp
--- src/BookmarkMenu.cpp
+++ src/BookmarkMenu.cpp
@@ -1,22 +1,9 @@
 /*  This file was part of the KDE libraries
 
-    Copyright 2019 by Tomaz Canabrava <tcanabrava@kde.org>
-    Copyright 2019 by Martin Sandsmark <martin.sandsmark@kde.org>
+    SPDX-FileCopyrightText: 2019 Tomaz Canabrava <tcanabrava@kde.org>
+    SPDX-FileCopyrightText: 2019 Martin Sandsmark <martin.sandsmark@kde.org>
 
-    library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation, version 2
-    or ( at your option ), any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+    SPDX-License-Identifier: GPL-2.0-or-later
 */
 
 // Own
