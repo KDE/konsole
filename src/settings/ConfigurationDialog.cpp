05c908e2a342fd6e9152cb794f2c85ce4e12597a
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2020-11-12 21:52:24 -0500

    Convert settings to use SPDX license/copyright
    https://community.kde.org/Policies/Licensing_Policy#SPDX_Statements
    https://community.kde.org/Guidelines_and_HOWTOs/Licensing

--- src/settings/ConfigurationDialog.cpp
+++ src/settings/ConfigurationDialog.cpp
@@ -1,25 +1,12 @@
 /*
-    Copyright 2019 by Mariusz Glebocki <mglb@arccos-1.net>
+    SPDX-FileCopyrightText: 2019 Mariusz Glebocki <mglb@arccos-1.net>
 
     Based on KConfigDialog from KConfigWidgets
 
-    Copyright (C) 2003 Benjamin C Meyer (ben+kdelibs at meyerhome dot net)
-    Copyright (C) 2003 Waldo Bastian <bastian@kde.org>
+    SPDX-FileCopyrightText: 2003 Benjamin C Meyer (ben+kdelibs at meyerhome dot net)
+    SPDX-FileCopyrightText: 2003 Waldo Bastian <bastian@kde.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-    02110-1301  USA.
+    SPDX-License-Identifier: GPL-2.0-or-later
 */
 
 // Own
