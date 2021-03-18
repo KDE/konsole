027e8c150c45ad1821d828ea93545a36f7b0a5f3
Luis Javier Merino Morán <ninjalj@gmail.com>
2021-03-18 20:08:11 +0100

    Don't send move events for normal mouse tracking
    xterm's DECSET 1000 requests justs press and release notifications, so
    do not send move events on this mode.
    
    BUG: 302731

--- src/Vt102Emulation.cpp
+++ src/Vt102Emulation.cpp
@@ -1063,6 +1063,11 @@ void Vt102Emulation::sendMouseEvent(int cb, int cx, int cy, int eventType)
         return;
     }
 
+    // Don't send move/drag events if only press and release requested
+    if (eventType == 1 && getMode(MODE_Mouse1000)) {
+        return;
+    }
+
     // With the exception of the 1006 mode, button release is encoded in cb.
     // Note that if multiple extensions are enabled, the 1006 is used, so it's okay to check for only that.
     if (eventType == 2 && !getMode(MODE_Mouse1006)) {
