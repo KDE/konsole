KDE/konsole

commit 6667d96ed9d6c95eab4ed13449d4b134005ffb4b

TerminalDisplay: Limit mouse move events.
The terminal display mouse tracking is based on character boxes, i.e.,
unless the mouse moves out of a character box its position hasn't really
changed. So most of the logic in `TerminalDisplay::mouseMoveEvent` can
be skipped/omitted till the pointer crosses box boundaries.

Before my changes applications like Vim/Neovim couldn't do selection via
dragging with the left mouse button (there might be a bug there). I
cursorily checked other features like the color filter and URL
underlining, and they work.

src/terminalDisplay/TerminalDisplay.cpp
@@ -355,6 +355,8 @@ TerminalDisplay::TerminalDisplay(QWidget* parent)
     , _boldIntense(true)
     , _lines(1)
     , _columns(1)
+    , _prevCharacterLine(-1)
+    , _prevCharacterColumn(-1)
     , _usedLines(1)
     , _usedColumns(1)
     , _contentRect(QRect())
@@ -1413,6 +1415,13 @@ void TerminalDisplay::mouseMoveEvent(QMouseEvent* ev)
 
     auto [charLine, charColumn] = getCharacterPosition(ev->pos(), !_usesMouseTracking);
 
+    if (charLine == _prevCharacterLine && charColumn == _prevCharacterColumn) {
+        return;
+    }
+
+    _prevCharacterLine = charLine;
+    _prevCharacterColumn = charColumn;
+
     processFilters();
 
     _filterChain->mouseMoveEvent(this, ev, charLine, charColumn);
@@ -1422,7 +1431,7 @@ void TerminalDisplay::mouseMoveEvent(QMouseEvent* ev)
     }
 
     // if the program running in the terminal is interested in Mouse Tracking
-    // evnets then emit a mouse movement signal, unless the shift key is
+    // events then emit a mouse movement signal, unless the shift key is
     // being held down, which overrides this.
     if (_usesMouseTracking && !(ev->modifiers() & Qt::ShiftModifier)) {
         int button = 3;

src/terminalDisplay/TerminalDisplay.h
@@ -684,6 +684,11 @@ private:
     int _lines;      // the number of lines that can be displayed in the widget
     int _columns;    // the number of columns that can be displayed in the widget
 
+    // Character line and character column as per a previous call to
+    // getCharacterPosition() in mouseMoveEvent().
+    int _prevCharacterLine;
+    int _prevCharacterColumn;
+
     int _usedLines;  // the number of lines that are actually being used, this will be less
     // than 'lines' if the character image provided with setImage() is smaller
     // than the maximum image size which can be displayed

commit c9f77e527643b7eeb7459febabbe9d29f249c996

Restore AutoScrollHandler behavior
AutoScrollHandler allows to extend the selection by dragging the mouse
outside the display.  It does this by generating synthetic mouse move
events when the left mouse button is held down and the mouse is moved
outside of the display area.  This improves the user experience of
extending the selection to cover text that is scrolled outside the
display area.

Unfortunately, this was broken by commit
6667d96ed9d6c95eab4ed13449d4b134005ffb4b, which filters out mouse move
events if they don't change the character cell position.  So, modify the
filtering a bit, allowing mouse movements outside the display area to
pass through.

src/terminalDisplay/TerminalDisplay.cpp
@@ -1244,7 +1244,12 @@ void TerminalDisplay::mouseMoveEvent(QMouseEvent* ev)
 
     auto [charLine, charColumn] = getCharacterPosition(ev->pos(), !_usesMouseTracking);
 
-    if (charLine == _prevCharacterLine && charColumn == _prevCharacterColumn) {
+    // Ignore mouse movements that don't change the character position,
+    //   but don't ignore the ones generated by AutoScrollHandler (which
+    //   allow to extend the selection by dragging the mouse outside the
+    //   display).
+    if (charLine == _prevCharacterLine && charColumn == _prevCharacterColumn &&
+            contentsRect().contains(ev->pos())) {
         return;
     }
 