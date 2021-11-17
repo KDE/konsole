0a6bcae57cb197fd8c69cca7a51b80b43205e4b0
Tomaz Canabrava <tcanabrava@kde.org>
2021-11-17 19:38:05 -0300

    Fix nitpicks

--- src/autotests/CharacterTest.cpp
+++ src/autotests/CharacterTest.cpp
@@ -7,21 +7,21 @@
 void Konsole::CharacterTest::testCanBeGrouped()
 {
     // Test for Capital Latin
-    for (uint32_t c = U'A'; c <= U'Z'; c++) {
+    for (uint32_t c = U'A'; c <= U'Z'; ++c) {
         Konsole::Character uppercase_latin_char(c);
         const bool res = uppercase_latin_char.canBeGrouped(false, false);
-        QCOMPARE(res, true);
+        QVERIFY(res);
     }
 
     // Test for Non Capital Latin
-    for (uint32_t c = U'a'; c <= U'z'; c++) {
+    for (uint32_t c = U'a'; c <= U'z'; ++c) {
         Konsole::Character lowercase_latin_char(c);
         const bool res = lowercase_latin_char.canBeGrouped(false, false);
-        QCOMPARE(res, true);
+        QVERIFY(res);
     }
 
     // Test for Braille
-    for (uint32_t c = 0x2800; c <= 0x28ff; c++) {
+    for (uint32_t c = 0x2800; c <= 0x28ff; ++c) {
         Konsole::Character braille_char(c);
         const bool res = braille_char.canBeGrouped(false, false);
         QCOMPARE(res, false);
