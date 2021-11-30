b18d7c9d4fef8b75180a372916b8e3d53ee93707
Tomaz Canabrava <tcanabrava@kde.org>
2021-11-30 15:11:28 -0300

    Use the correct variable on the inner loop
    ~Someone~ should fix the Qt model view framework to work with
    range based for.

--- src/plugins/SSHManager/sshmanagermodel.cpp
+++ src/plugins/SSHManager/sshmanagermodel.cpp
@@ -120,7 +120,7 @@ bool SSHManagerModel::hasHost(const QString &host) const
     // runs in O(N), should be ok for the amount of data peophe have.
     for (int i = 0, end = invisibleRootItem()->rowCount(); i < end; i++) {
         QStandardItem *iChild = invisibleRootItem()->child(i);
-        for (int e = 0, end = iChild->rowCount(); i < end; e++) {
+        for (int e = 0, end = iChild->rowCount(); e < end; e++) {
             QStandardItem *eChild = iChild->child(e);
             const auto data = eChild->data(SSHManagerModel::Roles::SSHRole).value<SSHConfigurationData>();
             if (data.host == host) {
