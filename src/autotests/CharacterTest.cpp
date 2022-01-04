13031aa37cc7ad65845515003478ef44a84af2cb
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2022-01-04 18:11:36 -0500

    Add license/copyright; copied from CharacterTest.h

--- src/autotests/CharacterTest.cpp
+++ src/autotests/CharacterTest.cpp
@@ -1,3 +1,9 @@
+/*
+    SPDX-FileCopyrightText: 2019 Tomaz Canabrava <tomaz.canabrava@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
 #include "CharacterTest.h"
 #include "Character.h"
 
