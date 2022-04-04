336e310fab88646ae32a58614592b48258d9d6a4
Matan Ziv-Av <matan@svgalib.org>
2022-04-04 15:23:44 +0300

    Scroll when a drawn sixel extends beyond bottom of line
    Commit e0b4b96a85a681fc56d9ebf9cdc0f8cc58c78c53 introduced a bug where the cursor did not move when drawing a sixel image, even when sixel scrolling mode is set.

--- src/Vt102Emulation.cpp
+++ src/Vt102Emulation.cpp
@@ -2343,7 +2343,7 @@ void Vt102Emulation::SixelModeDisable()
         pixmap = pixmap.scaled(pixmap.width(), m_aspect.first * pixmap.height() / m_aspect.second);
     }
     int rows = -1, cols = -1;
-    int needScroll = _currentScreen->addPlacement(pixmap, rows, cols, row, col, m_SixelScrolling, false, false);
+    int needScroll = _currentScreen->addPlacement(pixmap, rows, cols, row, col, m_SixelScrolling, m_SixelScrolling, false);
     if (m_SixelScrolling) {
         if (rows - needScroll > 0) {
             _currentScreen->cursorDown(rows - needScroll);
