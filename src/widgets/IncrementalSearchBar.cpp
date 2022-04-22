f813c334153a87db40b213ff52893a31d8f1af15
Ahmad Samir <a.samirh78@gmail.com>
2022-04-22 22:54:15 +0200

    Fix KStatefulBrush include when building without deprecated code
    It's been split to a separate header in KConfigWidgets 5.93.

--- src/widgets/IncrementalSearchBar.cpp
+++ src/widgets/IncrementalSearchBar.cpp
@@ -17,7 +17,14 @@
 
 // KDE
 #include "KonsoleSettings.h"
+
+#include <kconfigwidgets_version.h>
+#if KCONFIGWIDGETS_VERSION >= QT_VERSION_CHECK(5, 93, 0)
+#include <KStatefulBrush>
+#else
 #include <KColorScheme>
+#endif
+
 #include <KLocalizedString>
 #include <QLineEdit>
 
