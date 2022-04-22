260abc1ec74845e6f79c913f3b56fea94f50dd23
Ahmad Samir <a.samirh78@gmail.com>
2022-04-22 22:10:46 +0200

    Q_DECLARE_METATYPE isn't needed for Qt6 QExplicitlySharedDataPointer
    src/profile/Profile.h:815:1: error: specialization of ‘QMetaTypeId<QExplicitlySharedDataPointer<Konsole::Profile> >’ after instantiation
      815 | Q_DECLARE_METATYPE(Konsole::Profile::Ptr)
          | ^~~~~~~~~~~~~~~~~~
    src/profile/Profile.h:815:1: error: redefinition of ‘struct QMetaTypeId<QExplicitlySharedDataPointer<Konsole::Profile> >’
    /usr/include/qt6/QtCore/qmetatype.h:1124:8: note: previous definition of ‘struct QMetaTypeId<QExplicitlySharedDataPointer<Konsole::Profile> >’
     1124 | struct QMetaTypeId : public QMetaTypeIdQObject<T>

--- src/profile/Profile.h
+++ src/profile/Profile.h
@@ -809,6 +809,9 @@ inline QVariant Profile::property(Property p) const
 }
 
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
 Q_DECLARE_METATYPE(Konsole::Profile::Ptr)
+#endif
 
 #endif // PROFILE_H
