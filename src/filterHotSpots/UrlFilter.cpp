65d5d45c18493deebb3b5a8581ac167d228b2563
Ahmad Samir <a.samirh78@gmail.com>
2022-05-02 17:49:26 +0200

    Use possessive quantifiers to optimise regex matching performance
    See 'ATOMIC GROUPING AND POSSESSIVE QUANTIFIERS' section at:
    https://pcre.org/pcre.txt
    
    Thanks to Giuseppe D'Angelo for pointing that out.

--- src/filterHotSpots/UrlFilter.cpp
+++ src/filterHotSpots/UrlFilter.cpp
@@ -19,11 +19,17 @@ using namespace Konsole;
 // FullUrlRegExp is implemented based on:
 // https://datatracker.ietf.org/doc/html/rfc3986
 // See above URL for what "unreserved", "pct-encoded" ...etc mean, also
-// for the regex used for each part of the url being matched against
-
-// unreserved / pct-encoded / sub-delims
-// [a-z0-9\\-._~%!$&'()*+,;=]
-// The above string is used in various char[] below
+// for the regex used for each part of the url being matched against.
+//
+// It deviates from rfc3986:
+// - We only recognize URIs with authority (even if it is an empty authority)
+// - We match URIs starting with 'www.'
+// - "userinfo" is assumed to have a single ':' character
+// - We _don't_ match IPv6 addresses (e.g. http://[2010:836B:4179::836B:4179])
+//   or IPvFuture
+// - "port" (':1234'), if present, is assumed to be non-empty
+// - We don't check the validity of percent-encoded characters
+//   (e.g. "www.example.com/foo%XXbar")
 
 // All () groups are non-capturing (by using "(?:)" notation)
 // less bookkeeping on the PCRE engine side
@@ -31,32 +37,37 @@ using namespace Konsole;
 // scheme://
 // - Must start with an ASCII letter, preceeded by any non-word character,
 //   so "http" but not "mhttp"
-static const char scheme[] = "(?<=^|\\s|\\W)(?:[a-z][a-z0-9+\\-.]*://)";
+static const char scheme_or_www[] = "(?<=^|[\\s\\[\\]()'\"])(?:www\\.|[a-z][a-z0-9+\\-.]*+://)";
 
+// unreserved / pct-encoded / sub-delims
+#define COMMON_1 "a-z0-9\\-._~%!$&'()*+,;="
+
+/* clang-format off */
 // user:password@
 static const char userInfo[] =
     "(?:"
-    "[a-z0-9\\-._~%!$&'()*+,;=]+?:?"
-    "[a-z0-9\\-._~%!$&'()*+,;=]+@"
-    ")?";
-static const char host[] = "(?:[a-z0-9\\-._~%!$&'()*+,;=]+)"; // www.foo.bar
-static const char port[] = "(?::[0-9]+)?"; // :1234
-static const char path[] = "(?:[a-zA-Z0-9\\-._~%!$&'()*+,;=:@/]+)?"; // /path/to/some/place
-static const char query[] = "(?:\\?[a-z0-9\\-._~%!$&'()*+,;=:@/]+)?"; // "?somequery=bar"
-static const char fragment[] = "(?:#[a-z0-9/?]+)?";
+    "[" COMMON_1 "]+?:?"
+    "[" COMMON_1 "]++@"
+    ")?+";
+static const char host[] = "(?:[" COMMON_1 "]*+)"; // www.foo.bar
+static const char port[] = "(?::[0-9]+)?+"; // :1234
+
+#define COMMON_2 "a-z0-9\\-._~%!$&'()*+,;=:@/"
+static const char path[] = "(?:/[" COMMON_2 "]+)?+"; // /path/to/some/place
+static const char query[] = "(?:\\?[" COMMON_2 "]+)?+"; // "?somequery=bar"
+static const char fragment[] = "(?:#[" COMMON_2 "]+)?+"; // "#fragment"
 
 using LS1 = QLatin1String;
 
-/* clang-format off */
 const QRegularExpression UrlFilter::FullUrlRegExp(
-    LS1(scheme)
+    LS1(scheme_or_www)
     + LS1(userInfo)
     + LS1(host)
     + LS1(port)
     + LS1(path)
     + LS1(query)
     + LS1(fragment)
-    );
+    , QRegularExpression::CaseInsensitiveOption);
 /* clang-format on */
 
 /////////////////////////////////////////////
