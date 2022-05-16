KDE/konsole

commit c3b3ef19050b55e2dc7fc86cbf3efd1bb3f51601

RIS goes to (1,1) and clears tab stops
RIS (ESC c) - Hard Reset should move the cursor to (1,1) and clear tab
stops.

From esctest:
RISTests.test_RIS_CursorToOrigin
RISTests.test_RIS_ResetTabs

src/Screen.cpp
@@ -754,6 +754,7 @@ void Screen::reset()
     // Clear screen, but preserve the current line
     scrollUp(0, _cuY);
     _cuY = 0;
+    _cuX = 0;
 
     _currentModes[MODE_Origin] = 0;
     _savedModes[MODE_Origin] = 0;
@@ -774,6 +775,7 @@ void Screen::reset()
     // Other terminal emulators reset the entire scroll history during a reset
     //    setScroll(getScroll(), false);
 
+    initTabStops();
     setDefaultRendition();
     saveCursor();
 }

commit 9dadb7b562e9ecbffa107c9fcd2616661dbe626f

Fix regression preserving tcsh command prompt
Commit c3b3ef19 introduced a regression when invoking the
clear-history-and-reset action.  While RIS (Reset to Initial State) is
specified in DEC STD-070 as homing the cursor, konsole has grown some
hacks in the name of usability to preserve the command prompt line.

For a long time, it has sent two SIGWINCH with changed sizes after clear
and reset actions to force the shell to redraw the prompt (see d346a2cc,
temporarily disabled on 5d61b69e and re-added on 82778e87), which works
for bash, zsh, ksh, ...

tcsh doesn't redraw its prompt on SIGWINCH, but commit b8e96bcd modified
Screen::refresh() so instead of clearing the entire screen and homing
the cursor, it scrolled up everything but the last (usually the prompt)
line.

So, keep that last hack when called from clear-history-and-reset, and
behave as specified on DEC STD-070 otherwise.

Note that other ways of clearing the screen don't need hacks, e.g.
Ctrl-L, if handled at all, is handled by the shell, which then redraws
its prompt.  Calling "clear" or invoking "printf '\ec'" will result in
the shell redrawing its prompt in the usual way.

BUG: 453568

src/Emulation.h
@@ -177,8 +177,9 @@ public:
 
     /** Resets the state of the terminal.
      *
-     * @param softReset The reset was initiated by DECSTR */
-    virtual void reset(bool softReset = false) = 0;
+     * @param softReset The reset was initiated by DECSTR
+     * @param preservePrompt Try to preserve the command prompt */
+    virtual void reset(bool softReset = false, bool preservePrompt = false) = 0;
 
     /**
      * Returns true if the active terminal program is interested in Mouse

src/Screen.cpp
@@ -780,23 +780,28 @@ int Screen::getScreenLineColumns(const int line) const
     return _columns;
 }
 
-void Screen::reset(bool softReset)
+void Screen::reset(bool softReset, bool preservePrompt)
 {
     // Clear screen, but preserve the current line
     if (!softReset) {
-        scrollUp(0, _cuY);
-        _cuY = 0;
-        _cuX = 0;
+        if (preservePrompt) {
+            scrollUp(0, _cuY);
+            _cuY = 0;
+            if (_hasGraphics) {
+                delPlacements();
+                _currentTerminalDisplay->update();
+            }
+        } else {
+            clearEntireScreen();
+            _cuY = 0;
+            _cuX = 0;
+        }
 
         resetMode(MODE_Screen); // screen not inverse
         resetMode(MODE_NewLine);
 
         initTabStops();
 
-        if (_hasGraphics) {
-            delPlacements();
-            _currentTerminalDisplay->update();
-        }
     }
 
     _currentModes[MODE_Origin] = 0;

src/Screen.h
@@ -357,10 +357,13 @@ public:
      * <li>New line mode is disabled.  TODO Document me</li>
      * </ul>
      *
-     * If @p clearScreen is true then the screen contents are erased entirely,
-     * otherwise they are unaltered.
+     * If @p softReset is true then perform a DECSTR,
+     * otherwise perform RIS (Reset to Initial State).
+     *
+     * If @p preservePrompt is true, then attempt to preserve the
+     * line with the command prompt even on a RIS.
      */
-    void reset(bool softReset = false);
+    void reset(bool softReset = false, bool preservePrompt = false);
 
     /**
      * Displays a new character at the current cursor position.

src/Vt102Emulation.cpp
@@ -97,7 +97,7 @@ void Vt102Emulation::clearHistory()
     Emulation::clearHistory();
 }
 
-void Vt102Emulation::reset(bool softReset)
+void Vt102Emulation::reset(bool softReset, bool preservePrompt)
 {
     // Save the current codec so we can set it later.
     // Ideally we would want to use the profile setting
@@ -114,9 +114,9 @@ void Vt102Emulation::reset(bool softReset)
     }
 
     resetCharset(0);
-    _screen[0]->reset(softReset);
+    _screen[0]->reset(softReset, preservePrompt);
     resetCharset(1);
-    _screen[1]->reset(softReset);
+    _screen[1]->reset(softReset, preservePrompt);
 
     if (currentCodec != nullptr) {
         setCodec(currentCodec);

src/Vt102Emulation.h
@@ -73,7 +73,7 @@ public:
 
     // reimplemented from Emulation
     void clearEntireScreen() override;
-    void reset(bool softReset = false) override;
+    void reset(bool softReset = false, bool preservePrompt = false) override;
     char eraseChar() const override;
 
 public Q_SLOTS:

src/session/SessionController.cpp
@@ -1684,7 +1684,7 @@ void SessionController::clearHistoryAndReset()
     QByteArray name = profile->defaultEncoding().toUtf8();
 
     Emulation *emulation = session()->emulation();
-    emulation->reset();
+    emulation->reset(false, true);
     session()->refresh();
     session()->setCodec(QTextCodec::codecForName(name));
     clearHistory();