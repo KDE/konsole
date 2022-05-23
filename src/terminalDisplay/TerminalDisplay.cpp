KDE/konsole

commit 6667d96ed9d6c95eab4ed13449d4b134005ffb4b

TerminalDisplay: Limit mouse move events.
The terminal display mouse tracking is based on character boxes, i.e.,
unless the mouse moves out of a character box its position hasn't really
changed. So most of the logic in `TerminalDisplay::mouseMoveEvent` can
be skipped/omitted till the pointer crosses box boundaries.

Before my changes applications like Vim/Neovim couldn't do selection via
dragging with the left mouse button (there might be a bug there). I
cursorily checked other features like the color filter and URL
underlining, and they work.

src/terminalDisplay/TerminalDisplay.cpp
@@ -355,6 +355,8 @@ TerminalDisplay::TerminalDisplay(QWidget* parent)
     , _boldIntense(true)
     , _lines(1)
     , _columns(1)
+    , _prevCharacterLine(-1)
+    , _prevCharacterColumn(-1)
     , _usedLines(1)
     , _usedColumns(1)
     , _contentRect(QRect())
@@ -1413,6 +1415,13 @@ void TerminalDisplay::mouseMoveEvent(QMouseEvent* ev)
 
     auto [charLine, charColumn] = getCharacterPosition(ev->pos(), !_usesMouseTracking);
 
+    if (charLine == _prevCharacterLine && charColumn == _prevCharacterColumn) {
+        return;
+    }
+
+    _prevCharacterLine = charLine;
+    _prevCharacterColumn = charColumn;
+
     processFilters();
 
     _filterChain->mouseMoveEvent(this, ev, charLine, charColumn);
@@ -1422,7 +1431,7 @@ void TerminalDisplay::mouseMoveEvent(QMouseEvent* ev)
     }
 
     // if the program running in the terminal is interested in Mouse Tracking
-    // evnets then emit a mouse movement signal, unless the shift key is
+    // events then emit a mouse movement signal, unless the shift key is
     // being held down, which overrides this.
     if (_usesMouseTracking && !(ev->modifiers() & Qt::ShiftModifier)) {
         int button = 3;

src/terminalDisplay/TerminalDisplay.h
@@ -684,6 +684,11 @@ private:
     int _lines;      // the number of lines that can be displayed in the widget
     int _columns;    // the number of columns that can be displayed in the widget
 
+    // Character line and character column as per a previous call to
+    // getCharacterPosition() in mouseMoveEvent().
+    int _prevCharacterLine;
+    int _prevCharacterColumn;
+
     int _usedLines;  // the number of lines that are actually being used, this will be less
     // than 'lines' if the character image provided with setImage() is smaller
     // than the maximum image size which can be displayed

commit 93cabe1f2305ee2b93ab854fd6c356b5e39c1fed

Restrict mouse movement to cells only for mousetracking.
After 6667d96e broke scrolling, and apparently neither c9f77e52 nor MR
!413 where able to totally fix it for all use cases, applying the
restriction to cells only when sending mouse tracking events should fix
all remaining warts.

src/terminalDisplay/TerminalDisplay.cpp
@@ -1271,17 +1271,6 @@ void TerminalDisplay::mouseMoveEvent(QMouseEvent *ev)
 
     auto [charLine, charColumn] = getCharacterPosition(ev->pos(), !usesMouseTracking());
 
-    // Ignore mouse movements that don't change the character position,
-    //   but don't ignore the ones generated by AutoScrollHandler (which
-    //   allow to extend the selection by dragging the mouse outside the
-    //   display).
-    if (charLine == _prevCharacterLine && charColumn == _prevCharacterColumn && contentsRect().contains(ev->pos())) {
-        return;
-    }
-
-    _prevCharacterLine = charLine;
-    _prevCharacterColumn = charColumn;
-
     processFilters();
 
     _filterChain->mouseMoveEvent(this, ev, charLine, charColumn);
@@ -1290,6 +1279,14 @@ void TerminalDisplay::mouseMoveEvent(QMouseEvent *ev)
     // events then emit a mouse movement signal, unless the shift key is
     // being held down, which overrides this.
     if (usesMouseTracking() && !(ev->modifiers() & Qt::ShiftModifier)) {
+        // Ignore mouse movements that don't change the character position.
+        if (charLine == _prevCharacterLine && charColumn == _prevCharacterColumn) {
+            return;
+        }
+
+        _prevCharacterLine = charLine;
+        _prevCharacterColumn = charColumn;
+
         if (!_readOnly) {
             int button = 3;
             if ((ev->buttons() & Qt::LeftButton) != 0u) {