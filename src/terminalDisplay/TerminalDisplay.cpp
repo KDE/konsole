KDE/konsole

commit 0752cf3d6261d60bbb20a62194556b2377548368

Also prevent an intermediate selection entry for triple click
If a release event is the result of a double click, don't copy immediately
to X11 selection. Instead, start a timer, and copy only if no third click
happens during the interval.

src/terminalDisplay/TerminalDisplay.cpp
@@ -1118,6 +1118,7 @@ void TerminalDisplay::mousePressEvent(QMouseEvent *ev)
     }
 
     if (_possibleTripleClick && (ev->button() == Qt::LeftButton)) {
+        _needCopyToX11Selection = false;
         mouseTripleClickEvent(ev);
         return;
     }
@@ -1476,7 +1477,16 @@ void TerminalDisplay::mouseReleaseEvent(QMouseEvent *ev)
             _screenWindow->clearSelection();
         } else {
             if (_actSel > 1) {
-                copyToX11Selection();
+                if (_possibleTripleClick) {
+                    _needCopyToX11Selection = true;
+                    QTimer::singleShot(QApplication::doubleClickInterval(), this, [this]() {
+                        if (_needCopyToX11Selection) {
+                            copyToX11Selection();
+                        }
+                    });
+                } else {
+                    copyToX11Selection();
+                }
             }
 
             _actSel = 0;

src/terminalDisplay/TerminalDisplay.h
@@ -730,6 +730,7 @@ private:
     Enum::TripleClickModeEnum _tripleClickMode = Enum::SelectWholeLine;
     bool _possibleTripleClick = false; // is set in mouseDoubleClickEvent and deleted
     // after QApplication::doubleClickInterval() delay
+    bool _needCopyToX11Selection = false; // set in mouseReleaseEvent after mouseDoubleClickEvent
 
     QLabel *_resizeWidget = nullptr;
     QTimer *_resizeTimer = nullptr;

commit b238fb29cf9fe061df8c79b07f5126346bff9c88

Fix bug in double-click selection
A bug introduced in 0752cf3d prevents copying selection to clipboard if
a key is pressed a short time after a double click. This is avoided by:
- Do not exit selection mode in the time interval between a double click
  and a (potential) third click.
- Save the selection text on a double click, and copy the saved text,
  rather than the current selection if no third click at the end of the
  triple click interval.

BUG: 463268

src/terminalDisplay/TerminalDisplay.cpp
@@ -1487,11 +1487,20 @@ void TerminalDisplay::mouseReleaseEvent(QMouseEvent *ev)
             if (_actSel > 1) {
                 if (_possibleTripleClick) {
                     _needCopyToX11Selection = true;
-                    QTimer::singleShot(QApplication::doubleClickInterval(), this, [this]() {
-                        if (_needCopyToX11Selection) {
-                            copyToX11Selection();
+                    const QString &text = _screenWindow->selectedText(currentDecodingOptions());
+                    if (!text.isEmpty()) {
+                        _savedMimeData = new QMimeData;
+                        _savedMimeData->setText(text);
+                        if (_copyTextAsHTML) {
+                            _savedMimeData->setHtml(_screenWindow->selectedText(currentDecodingOptions() | Screen::ConvertToHtml));
                         }
-                    });
+                        QTimer::singleShot(QApplication::doubleClickInterval(), this, [this]() {
+                            if (_needCopyToX11Selection) {
+                                copyToX11Selection(true);
+                            }
+                            _savedMimeData = nullptr;
+                        });
+                    }
                 } else {
                     copyToX11Selection();
                 }
@@ -2340,22 +2349,27 @@ void TerminalDisplay::setCopyTextAsHTML(bool enabled)
     _copyTextAsHTML = enabled;
 }
 
-void TerminalDisplay::copyToX11Selection()
+void TerminalDisplay::copyToX11Selection(bool useSavedText)
 {
     if (_screenWindow.isNull()) {
         return;
     }
 
-    const QString &text = _screenWindow->selectedText(currentDecodingOptions());
-    if (text.isEmpty()) {
-        return;
-    }
+    QMimeData *mimeData;
+    if (useSavedText) {
+        mimeData = _savedMimeData;
+    } else {
+        const QString &text = _screenWindow->selectedText(currentDecodingOptions());
+        if (text.isEmpty()) {
+            return;
+        }
 
-    auto mimeData = new QMimeData;
-    mimeData->setText(text);
+        mimeData = new QMimeData;
+        mimeData->setText(text);
 
-    if (_copyTextAsHTML) {
-        mimeData->setHtml(_screenWindow->selectedText(currentDecodingOptions() | Screen::ConvertToHtml));
+        if (_copyTextAsHTML) {
+            mimeData->setHtml(_screenWindow->selectedText(currentDecodingOptions() | Screen::ConvertToHtml));
+        }
     }
 
     if (QApplication::clipboard()->supportsSelection()) {
@@ -2744,8 +2758,10 @@ void TerminalDisplay::keyPressEvent(QKeyEvent *event)
 #endif
 
     if (!_readOnly) {
-        _actSel = 0; // Key stroke implies a screen update, so TerminalDisplay won't
-                     // know where the current selection is.
+        if (!_possibleTripleClick) {
+            _actSel = 0; // Key stroke implies a screen update, so TerminalDisplay won't
+                         // know where the current selection is.
+        }
 
         if (_allowBlinkingCursor) {
             _blinkCursorTimer->start();

src/terminalDisplay/TerminalDisplay.h
@@ -41,6 +41,7 @@ class QTimerEvent;
 class QScrollEvent;
 class QScrollPrepareEvent;
 class KMessageWidget;
+class QMimeData;
 
 struct UBiDi;
 
@@ -430,7 +431,7 @@ public Q_SLOTS:
     void updateImage();
 
     /** Copies the selected text to the X11 Selection. */
-    void copyToX11Selection();
+    void copyToX11Selection(bool useSavedText = false);
 
     /** Copies the selected text to the system clipboard. */
     void copyToClipboard(Screen::DecodingOptions options = Screen::PlainText);
@@ -733,6 +734,7 @@ private:
     bool _possibleTripleClick = false; // is set in mouseDoubleClickEvent and deleted
     // after QApplication::doubleClickInterval() delay
     bool _needCopyToX11Selection = false; // set in mouseReleaseEvent after mouseDoubleClickEvent
+    QMimeData *_savedMimeData;
 
     QLabel *_resizeWidget = nullptr;
     QTimer *_resizeTimer = nullptr;