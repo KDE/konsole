KDE/konsole

commit 97feb543975f7ae2ed2ab2b90ce9bdd348e10bed

Handle changing cursor shape property without creating a new profile
Using an escape sequence, e.g. printf '\e]50;CursorShape=1\a', to change
the cursor shape property like we handle DECSCUSR, i.e. as a transient
change without creating a new profile (see
SessionManager::sessionProfileCommandReceived()).

To test:
- printf '\e]50;CursorShape=1\a' to change the cursor shape, check that the
  current profile hasn't been switched to a new profile (that has no name
  as it's a temp profile)

BUG: 445590
FIXED-IN: 21.12

src/Vt102Emulation.cpp
@@ -544,6 +544,19 @@ void Vt102Emulation::processSessionAttributeRequest(int tokenSize)
         return;
     }
 
+    if (attribute == Session::ProfileChange) {
+        if (value.startsWith(QLatin1String("CursorShape="))) {
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+            const auto numStr = QStringView(value).right(1);
+#else
+            const auto numStr = value.rightRef(1);
+#endif
+            const Enum::CursorShapeEnum shape = static_cast<Enum::CursorShapeEnum>(numStr.toInt());
+            Q_EMIT setCursorStyleRequest(shape);
+            return;
+        }
+    }
+
     _pendingSessionAttributesUpdates[attribute] = value;
     _sessionAttributesUpdateTimer->start(20);
 }

commit 7a9d8839ad297c43a82d4003b1e9a5f48d5f33f8

Allow setting cursor color with escape sequence again.
This commit 97feb543 broke support for changing cursor color 
by escape sequence if value starts with `CursorShape=`.

printf '\e]50;CursorShape=0;CustomCursorColor=#ff5f00\a'

This also implements support for setting cursor color and also for cursor
blinking without changing the current profile what is purpose of the
97feb543 change.

src/Emulation.h
@@ -395,8 +395,10 @@ Q_SIGNALS:
      * to the terminal.
      * @p shape cursor shape
      * @p isBlinking if true, the cursor will be set to blink
+     * @p customColor custom cursor color
      */
-    void setCursorStyleRequest(Enum::CursorShapeEnum shape = Enum::BlockCursor, bool isBlinking = false);
+    void setCursorStyleRequest(Enum::CursorShapeEnum shape = Enum::BlockCursor, bool isBlinking = false, const QColor &customColor = {});
+
     /**
      * Emitted when reset() is called to reset the cursor style to the
      * current profile cursor shape and blinking settings.

src/Vt102Emulation.cpp
@@ -25,6 +25,7 @@
 // Konsole
 #include "EscapeSequenceUrlExtractor.h"
 #include "session/SessionController.h"
+#include "session/SessionManager.h"
 #include "terminalDisplay/TerminalColor.h"
 #include "terminalDisplay/TerminalDisplay.h"
 #include "terminalDisplay/TerminalFonts.h"
@@ -1146,12 +1147,76 @@ void Vt102Emulation::processSessionAttributeRequest(const int tokenSize, const u
     }
 
     if (attribute == Session::ProfileChange) {
-        if (value.startsWith(QLatin1String("CursorShape="))) {
-            const auto numStr = QStringView(value).right(1);
-            const Enum::CursorShapeEnum shape = static_cast<Enum::CursorShapeEnum>(numStr.toInt());
-            Q_EMIT setCursorStyleRequest(shape);
+        bool styleChanged = false;
+        bool isBlinking = false;
+        Enum::CursorShapeEnum shape = Enum::BlockCursor;
+        QColor customColor;
+
+        if (TerminalDisplay *currentView = _currentScreen->currentTerminalDisplay()) {
+            if (SessionController *sessionController = currentView->sessionController()) {
+                Session *session = sessionController->session();
+                SessionManager *manager = SessionManager::instance();
+                Profile::Ptr currentProfile = manager->sessionProfile(session);
+                shape = currentProfile->cursorShape();
+                isBlinking = currentProfile->blinkingCursorEnabled();
+                customColor = currentProfile->customCursorColor();
+            }
+        }
+
+        const QLatin1String cursorShapeStr("CursorShape=");
+        const QLatin1String blinkingCursorStr("BlinkingCursorEnabled=");
+        const QLatin1String customCursorColorStr("CustomCursorColor=");
+
+        QString newValue;
+        #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+        const auto items = QStringView(value).split(QLatin1Char(';'));
+        #else
+        const auto items = value.splitRef(QLatin1Char(';'));
+        #endif
+        for (const auto &item: items) {
+            if (item.startsWith(cursorShapeStr)) {
+                const int s = item.at(cursorShapeStr.size()).digitValue();
+                shape = static_cast<Enum::CursorShapeEnum>(s != -1 ? s : 0);
+                styleChanged = true;
+
+            } else if (item.startsWith(blinkingCursorStr)) {
+                const auto str = item.mid(blinkingCursorStr.size());
+                if (str.compare(QString::fromLatin1("true"), Qt::CaseInsensitive) == 0) {
+                    isBlinking = true;
+                    styleChanged = true;
+                } else if (str.compare(QString::fromLatin1("false"), Qt::CaseInsensitive) == 0) {
+                    isBlinking = false;
+                    styleChanged = true;
+                } else {
+                    bool ok = false;
+                    bool newIsBlinking = str.toInt(&ok);
+                    if (ok) {
+                        isBlinking = newIsBlinking;
+                        styleChanged = true;
+                    }
+                }
+
+            } else if (item.startsWith(customCursorColorStr)) {
+                const auto colorStr = item.mid(customCursorColorStr.size());
+                customColor = QColor(colorStr);
+                styleChanged = true;
+
+            } else {
+                if (!newValue.isEmpty())
+                    newValue.append(QChar::fromLatin1(';'));
+                newValue.append(item);
+            }
+        }
+
+        if (styleChanged) {
+            Q_EMIT setCursorStyleRequest(shape, isBlinking, customColor);
+        }
+
+        if (newValue.isEmpty()) {
             return;
         }
+
+        value = newValue;
     }
 
     if (attribute == Image) {

src/profile/Profile.h
@@ -17,7 +17,9 @@
 #include <QStringList>
 #include <QVariant>
 
+// Konsole
 #include "konsoleprivate_export.h"
+#include "Enumeration.h"
 
 #include <map>
 #include <vector>
@@ -699,6 +701,12 @@ public:
     bool flowControlEnabled() const
     {
         return property<bool>(Profile::FlowControlEnabled);
+    }
+
+    /** Convenience method for property<int>(Profile::CursorShape) */
+    Enum::CursorShapeEnum cursorShape() const
+    {
+        return static_cast<Enum::CursorShapeEnum>(property<int>(Profile::CursorShape));
     }
 
     /** Convenience method for property<bool>(Profile::UseCustomCursorColor) */

src/terminalDisplay/TerminalColor.cpp
@@ -84,6 +84,12 @@ QRgb TerminalColor::blendColor() const
     return m_blendColor;
 }
 
+void TerminalColor::setCursorColor(const QColor &color)
+{
+    m_cursorColor = color;
+    onColorsChanged();
+}
+
 void TerminalColor::setBackgroundColor(const QColor &color)
 {
     m_colorTable[DEFAULT_BACK_COLOR] = color;

src/terminalDisplay/TerminalColor.h
@@ -50,6 +50,8 @@ public:
         return m_cursorColor;
     }
 
+    void setCursorColor(const QColor &color);
+
     QColor cursorTextColor() const
     {
         return m_cursorTextColor;

src/terminalDisplay/TerminalDisplay.cpp
@@ -368,11 +368,15 @@ void TerminalDisplay::setKeyboardCursorShape(Enum::CursorShapeEnum shape)
     _cursorShape = shape;
 }
 
-void TerminalDisplay::setCursorStyle(Enum::CursorShapeEnum shape, bool isBlinking)
+void TerminalDisplay::setCursorStyle(Enum::CursorShapeEnum shape, bool isBlinking, const QColor &customColor)
 {
     setKeyboardCursorShape(shape);
 
     setBlinkingCursorEnabled(isBlinking);
+
+    if (customColor.isValid()) {
+        _terminalColor->setCursorColor(customColor);
+    }
 
     // when the cursor shape and blinking state are changed via the
     // Set Cursor Style (DECSCUSR) escape sequences in vim, and if the

src/terminalDisplay/TerminalDisplay.h
@@ -162,8 +162,10 @@ public:
      * Sets the Cursor Style (DECSCUSR) via escape sequences
      * @p shape cursor shape
      * @p isBlinking if true, the cursor will be set to blink
+     * @p customColor custom cursor color
      */
-    void setCursorStyle(Enum::CursorShapeEnum shape, bool isBlinking);
+    void setCursorStyle(Enum::CursorShapeEnum shape, bool isBlinking, const QColor &customColor);
+
     /**
      * Resets the cursor style to the current profile cursor shape and
      * blinking settings