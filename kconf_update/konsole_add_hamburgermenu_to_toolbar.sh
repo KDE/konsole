a7da80e02ab9c88157614a266ed94732c7e53233
Christophe Marin <christophe@krop.fr>
2024-03-11 18:12:12 +0100

    Don't rely on qtpaths for kconf_update scripts
    qtpaths is a developer tool and there's no guarantee the executable in $PATH is called 'qtpaths'
    
    (cherry picked from commit 479acabf9034c24bbd492e25d4358854f1c2828e)

--- kconf_update/konsole_add_hamburgermenu_to_toolbar.sh
+++ kconf_update/konsole_add_hamburgermenu_to_toolbar.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+if [ -z "$XDG_DATA_HOME" ]; then
+    XDG_DATA_HOME="$HOME/.local/share"
+fi
+
 sed --regexp-extended --null-data --in-place '
 s/(<ToolBar.*)(<\/ToolBar>)/\1<Action name="hamburger_menu"\/>\n\2/
 ' \
-`qtpaths --locate-file GenericDataLocation kxmlgui5/konsole/sessionui.rc`
+${XDG_DATA_HOME}/kxmlgui5/konsole/sessionui.rc
