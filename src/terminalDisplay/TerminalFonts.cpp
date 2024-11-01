be76d4a2480fbf24e261cf3bf0f69a781100d534
Matan Ziv-Av <matan@svgalib.org>
2024-11-01 19:19:22 +0200

    Revert "TerminalPainter: Fix font width on Qt 6"
    This reverts commit a6d366f1fb2e915ad8d0fb9b471a033ef35b7e37.
    
    Enabling hinting does not achieve the desired results (integer width fonts).
    
    It helps with some fonts, but makes the situation worse with others.

--- src/terminalDisplay/TerminalFonts.cpp
+++ src/terminalDisplay/TerminalFonts.cpp
@@ -79,10 +79,6 @@ void TerminalFont::setVTFont(const QFont &f)
     // Disabling kerning saves some computation when rendering text.
     newFont.setKerning(false);
 
-    // QFont::ForceIntegerMetrics has been removed.
-    // Set full hinting instead to ensure the letters are aligned properly.
-    newFont.setHintingPreference(QFont::PreferFullHinting);
-
     // "Draw intense colors in bold font" feature needs to use different font weights. StyleName
     // property, when set, doesn't allow weight changes. Since all properties (weight, stretch,
     // italic, etc) are stored in QFont independently, in almost all cases styleName is not needed.
