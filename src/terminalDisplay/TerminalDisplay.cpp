6fa097fd28997d64e62420be8666b94e7dfd671b
Harald Sitter <sitter@kde.org>
2026-01-28 22:11:04 +0100

    terminaldisplay: do not explode on poor dirty indices
    when the cursor is not at the end of the dirty region we would sometimes
    try to get a negative range where the start is after the end. to prevent
    this let's accurately track the range of dirty indices and request
    those.
    as additional safety net we also make sure to bound the values, lest we
    request too much data and tap into invalid memory

--- src/terminalDisplay/TerminalDisplay.cpp
+++ src/terminalDisplay/TerminalDisplay.cpp
@@ -519,7 +519,8 @@ void TerminalDisplay::updateImage()
     auto dirtyMask = new char[columnsToUpdate + 2];
     QRegion dirtyRegion;
 
-    std::optional<int> startIndex;
+    std::optional<int> startDirtyIndex;
+    std::optional<int> endDirtyIndex;
 
     for (y = 0; y < linesToUpdate; ++y) {
         const Character *currentLine = &_image[y * _columns];
@@ -536,10 +537,11 @@ void TerminalDisplay::updateImage()
             if (newLine[x] != currentLine[x]) {
                 dirtyMask[x] = 1;
 
-                if (!startIndex) {
-                    const int lineStartIndex = (y * columns) + x;
-                    startIndex = lineStartIndex;
+                const int indexInImage = (y * columns) + x;
+                if (!startDirtyIndex) {
+                    startDirtyIndex = indexInImage;
                 }
+                endDirtyIndex = indexInImage;
             }
         }
 
@@ -663,13 +665,17 @@ void TerminalDisplay::updateImage()
     QAccessibleTextCursorEvent cursorEvent(this, _usedColumns * screenWindow()->screen()->getCursorY() + screenWindow()->screen()->getCursorX());
     QAccessible::updateAccessibility(&cursorEvent);
 
-    if (startIndex) {
-        const auto currentCursor = [&]() -> int {
-            int offset = _usedColumns * screenWindow()->screen()->getCursorY();
-            return offset + screenWindow()->screen()->getCursorX();
-        };
+    if (startDirtyIndex && endDirtyIndex) {
+        const auto maxIndex = _usedLines * _usedColumns;
 
-        const QString newText = screenWindow()->screen()->text(*startIndex, currentCursor(), Screen::PreserveLineBreaks);
+        // Ensure our indices are in-bounds. It can happen that we have more dirty cells than are on the display. Notably when shrinking the window
+        // we may have output stream past the current Display region. Let's make sure we aren't tapping into invalid inices when that happens.
+        startDirtyIndex = qBound(0, *startDirtyIndex, maxIndex);
+        endDirtyIndex = qBound(*startDirtyIndex, *endDirtyIndex, maxIndex);
+
+        Q_ASSERT(endDirtyIndex <= maxIndex);
+        Q_ASSERT(endDirtyIndex >= startDirtyIndex);
+        const QString newText = screenWindow()->screen()->text(*startDirtyIndex, *endDirtyIndex, Screen::PreserveLineBreaks);
         QAccessibleTextUpdateEvent textUpdateEvent(this, 0, QString(), newText);
         QAccessible::updateAccessibility(&textUpdateEvent);
     }
